31d70c54d1af045cfa7d6726e97eb9b1759828c0
Javier Serrano <js@mega.co.nz>
2013-12-29 04:03:26 +0100

    Changes to build with Visual Studio Express

--- Source/MEGAShellExt/MEGAShellExt.pro
+++ Source/MEGAShellExt/MEGAShellExt.pro
@@ -14,7 +14,7 @@ LIBS += -luser32 -lole32 -loleaut32 -lgdi32 -luuid -lAdvapi32 -lShell32
 DEF_FILE = GlobalExportFunctions.def
 RC_FILE = MEGAShellExt.rc
 
-OTHER_FILES += GlobalExportFunctions.def
+OTHER_FILES += GlobalExportFunctions.def MEGAShellExt.rc
 
 HEADERS += \
     ShellExt.h \
--- Source/MEGAShellExt/resource.h
+++ Source/MEGAShellExt/resource.h
@@ -1,19 +1,4 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by MEGAShellExt.rc
-//
 #define IDI_ICON1                       105
 #define IDI_ICON2                       106
 #define IDI_ICON3                       107
 #define IDI_ICON4                       108
-
-// Next default values for new objects
-//
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        114
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
