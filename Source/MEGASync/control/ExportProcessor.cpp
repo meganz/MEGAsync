40996fdf79c513fa3cb10a8bd73e13ef73efa3ec
Javier Serrano <js@mega.co.nz>
2014-01-19 20:53:48 +0100

    Fix getting Mega links

--- Source/MEGASync/control/ExportProcessor.cpp
+++ Source/MEGASync/control/ExportProcessor.cpp
@@ -16,6 +16,11 @@ void ExportProcessor::requestLinks()
 {
     for(int i=0; i<fileList.size(); i++)
     {
+        if(!fileList[i].startsWith(QString::fromAscii("\\\\?\\")))
+            fileList[i].insert(0, QString::fromAscii("\\\\?\\"));
+
+        cout << fileList[i].toStdString() << endl;
+
 #ifdef WIN32
         string tmpPath((const char*)fileList[i].utf16(), fileList[i].size()*sizeof(wchar_t));
 #else
