dae9511a4f22205024084f61e0e1fdd7685f70d9
Javier Serrano <js@mega.co.nz>
2014-01-30 03:06:08 +0100

    Version to get feedback about one bug

--- Source/MEGASync/MegaApplication.cpp
+++ Source/MEGASync/MegaApplication.cpp
@@ -13,7 +13,7 @@
 #include <QFontDatabase>
 #include <QNetworkProxy>
 
-const int MegaApplication::VERSION_CODE = 1004;
+const int MegaApplication::VERSION_CODE = 1003;
 const QString MegaApplication::VERSION_STRING = QString::fromAscii("1.0.4c");
 const QString MegaApplication::TRANSLATION_FOLDER = QString::fromAscii("://translations/");
 const QString MegaApplication::TRANSLATION_PREFIX = QString::fromAscii("MEGASyncStrings_");
