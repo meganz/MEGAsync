da1900fd7cbf9f904907c6b71535c08c1cbaf96e
Javier Serrano <js@mega.co.nz>
2014-01-30 18:53:17 +0100

    Test version 1.0.4d

--- Source/MEGASync/MegaApplication.cpp
+++ Source/MEGASync/MegaApplication.cpp
@@ -14,7 +14,7 @@
 #include <QNetworkProxy>
 
 const int MegaApplication::VERSION_CODE = 1003;
-const QString MegaApplication::VERSION_STRING = QString::fromAscii("1.0.4c");
+const QString MegaApplication::VERSION_STRING = QString::fromAscii("1.0.4d");
 const QString MegaApplication::TRANSLATION_FOLDER = QString::fromAscii("://translations/");
 const QString MegaApplication::TRANSLATION_PREFIX = QString::fromAscii("MEGASyncStrings_");
 
