7dc44feee07c22c17995e6b8eaf1dd767bbc4ed0
Javier Serrano <js@mega.co.nz>
2014-01-31 03:04:12 +0100

    Test installer 1.0.4g without log messages

--- Source/MEGASync/MegaApplication.cpp
+++ Source/MEGASync/MegaApplication.cpp
@@ -14,7 +14,7 @@
 #include <QNetworkProxy>
 
 const int MegaApplication::VERSION_CODE = 1003;
-const QString MegaApplication::VERSION_STRING = QString::fromAscii("1.0.4f");
+const QString MegaApplication::VERSION_STRING = QString::fromAscii("1.0.4g");
 const QString MegaApplication::TRANSLATION_FOLDER = QString::fromAscii("://translations/");
 const QString MegaApplication::TRANSLATION_PREFIX = QString::fromAscii("MEGASyncStrings_");
 
