ef4b9b46292e5929cdbedf60052c113dcdcc1551
Javier Serrano <js@mega.co.nz>
2014-01-31 16:31:03 +0100

    Version 1.0.4
    New version code.
    New user agent.
    Minimal crash reports enabled.

--- Source/MEGASync/sdk/megaapi.cpp
+++ Source/MEGASync/sdk/megaapi.cpp
@@ -980,7 +980,7 @@ MegaApi::MegaApi(MegaListener *listener, string *basePath)
     waiter = new MegaWaiter();
     fsAccess = new MegaFileSystemAccess();
     dbAccess = new MegaDbAccess(basePath);
-    client = new MegaClient(this, waiter, httpio, fsAccess, dbAccess, "FhMgXbqb", "MEGAsync/1.0.3");
+    client = new MegaClient(this, waiter, httpio, fsAccess, dbAccess, "FhMgXbqb", "MEGAsync/1.0.4");
 
     //Start blocking thread
 	threadExit = 0;
