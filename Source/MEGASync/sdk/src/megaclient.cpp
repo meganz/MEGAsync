5b2826921d310f8200d9ae455961b1f37f748c3e
Javier Serrano <js@mega.co.nz>
2014-02-14 15:43:33 +0100

    Fixed an access to invalid iterators
    In the Node destructor, iterators that belong to the "todebris" set are
    accessed. In MegaClient::purgenodesusersabortsc(), the "todebris" set
    was cleared before the deletion of nodes.

--- Source/MEGASync/sdk/src/megaclient.cpp
+++ Source/MEGASync/sdk/src/megaclient.cpp
@@ -4502,13 +4502,12 @@ void MegaClient::purgenodesusersabortsc()
 
     syncs.clear();
 
-    todebris.clear();
-
     for (node_map::iterator it = nodes.begin(); it != nodes.end(); it++)
     {
         delete it->second;
     }
 
+    todebris.clear();
     nodes.clear();
 
     for (newshare_list::iterator it = newshares.begin(); it != newshares.end(); it++)
