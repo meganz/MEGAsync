f01dee16030bf33966ee9345d950855216442919
Javier Serrano <js@mega.co.nz>
2014-03-22 03:00:56 +0100

    SDK update

--- Source/MEGASync/sdk/src/node.cpp
+++ Source/MEGASync/sdk/src/node.cpp
@@ -52,7 +52,7 @@ Node::Node(MegaClient* cclient, node_vector* dp, handle h, handle ph,
 
     size = s;
     owner = u;
-
+    
     copystring(&fileattrstring, fa);
 
     clienttimestamp = tm;
@@ -62,6 +62,7 @@ Node::Node(MegaClient* cclient, node_vector* dp, handle h, handle ph,
     sharekey = NULL;
 
     removed = 0;
+    memset(&changed,0,sizeof changed);
 
     if (client)
     {
