019e2eae3a3444ca13d2ca071c7d77006de305d4
javiserrano <fjaviersr@gmail.com>
2014-03-23 03:20:05 +0100

    Fixed Ubuntu builds

--- create_tarball.sh
+++ create_tarball.sh
@@ -6,6 +6,7 @@ rm -rf megasync-$MEGASYNC_VERSION
 mkdir megasync-$MEGASYNC_VERSION
 ln -s ../megasync.spec megasync-$MEGASYNC_VERSION/megasync.spec
 ln -s ../Source/configure megasync-$MEGASYNC_VERSION/configure
+ln -s ../curl-7.32.0.tar.gz megasync-$MEGASYNC_VERSION/curl-7.32.0.tar.gz
 ln -s ../Source/MEGA.pro megasync-$MEGASYNC_VERSION/MEGA.pro
 ln -s ../Source/MEGASync megasync-$MEGASYNC_VERSION/MEGASync
 ln -s ../Source/MEGAShellExtNautilus megasync-$MEGASYNC_VERSION/MEGAShellExtNautilus
