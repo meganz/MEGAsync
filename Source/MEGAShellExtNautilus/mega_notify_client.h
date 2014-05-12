c23c49bb487b8072a7937b48bdc01ad4bd0f3ac6
Paul Ionkin <paul.ionkin@gmail.com>
2014-05-12 06:08:33 +0200

    Optimize number of requests. Add Notify client

---
+++ Source/MEGAShellExtNautilus/mega_notify_client.h
@@ -0,0 +1,9 @@
+#ifndef MEGA_NOTIFY_CLIENT_H
+#define MEGA_NOTIFY_CLIENT_H
+
+#include "MEGAShellExt.h"
+
+void mega_notify_client_timer_start(MEGAExt *mega_ext);
+void mega_notify_client_destroy(MEGAExt *mega_ext);
+
+#endif
