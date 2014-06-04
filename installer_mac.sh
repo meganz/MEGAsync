e7d9cef88ce9bdbd07823915e9e16b26cf07d9c1
Victor Teniente <vtmateos@gmail.com>
2014-06-04 18:14:54 +0200

    Fixed Mac build script for QT5

--- installer_mac.sh
+++ installer_mac.sh
@@ -3,9 +3,9 @@
 rm -rf Release_x64
 mkdir Release_x64
 cd Release_x64
-qmake -r ../Source -spec macx-g++ CONFIG+=release CONFIG+=x86_64 -nocache
+~/Qt5.3.0/5.3/clang_64/bin/qmake -r ../Source -spec macx-g++ CONFIG+=release CONFIG+=x86_64 -nocache
 make
-macdeployqt MEGASync/MEGAsync.app
+~/Qt5.3.0/5.3/clang_64/bin/macdeployqt MEGASync/MEGAsync.app
 dsymutil MEGASync/MEGAsync.app/Contents/MacOS/MEGAsync -o MEGAsync.app.dSYM
 strip MEGASync/MEGAsync.app/Contents/MacOS/MEGAsync
 dsymutil MEGALoader/MEGAloader.app/Contents/MacOS/MEGAloader -o MEGAloader.dSYM
