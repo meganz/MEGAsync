5a445ae73b8713fe0d926345d604815452e8a390
Victor Teniente <vtmateos@gmail.com>
2014-06-06 14:51:32 +0200

    Test with popup windows

--- Source/MEGASync/gui/InfoDialog.cpp
+++ Source/MEGASync/gui/InfoDialog.cpp
@@ -539,10 +539,10 @@ void InfoDialog::on_bSyncFolder_clicked()
     else
     {
         QMenu menu;
-        menu.setStyleSheet(QString::fromAscii(
-            "QMenu {background-color: white; border: 2px solid #B8B8B8; padding: 5px; border-radius: 5px;} "
-            "QMenu::item {background-color: white; color: black;} "
-            "QMenu::item:selected {background-color: rgb(242, 242, 242);}"));
+        //menu.setStyleSheet(QString::fromAscii(
+           // "QMenu {} "));
+           // "QMenu::item {background-color: white; color: black;} "
+           // "QMenu::item:selected {background-color: #0063cd; color:white}"));
         QAction *addSyncAction = menu.addAction(tr("Add Sync"), this, SLOT(addSync()));
         addSyncAction->setIcon(QIcon(QString::fromAscii("://images/tray_add_sync_ico.png")));
         addSyncAction->setIconVisibleInMenu(true);
