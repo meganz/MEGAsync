6aee553635b7f7907cca087b84a9b3fde7221393
Victor Teniente <vtmateos@gmail.com>
2014-08-08 12:18:51 +0200

    Initial commit of MEGAsync Debug
    Application to communicate through local sockets with MEGAsync. In this
    manner, we can log all debug messages in this application although
    MEGAsync crashes. Furthermore, we can export/import logs to compress
    files in order to analyze later. The program includes specific features
    to facilitate analysis of log reports.

---
+++ Source/MEGAsyncDebug/MegaDebugServer.h
@@ -0,0 +1,67 @@
+#ifndef MEGADEBUGSERVER_H
+#define MEGADEBUGSERVER_H
+
+#include <QMainWindow>
+#include <QLocalServer>
+#include <QLocalSocket>
+#include <QObject>
+#include <QSortFilterProxyModel>
+#include <QStandardItemModel>
+#include <QXmlStreamReader>
+#include <QFileDialog>
+#include <QMessageBox>
+
+struct DebugRow
+{
+    QString timeStamp;
+    QString messageType;
+    QString content;
+
+};
+
+namespace Ui {
+class MegaDebugServer;
+}
+
+class MegaDebugServer : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MegaDebugServer(QWidget *parent = 0);
+
+    ~MegaDebugServer();
+
+private:
+    Ui::MegaDebugServer *ui;
+    QLocalServer *megaServer;
+    QLocalSocket *megaSyncClient;
+    QXmlStreamReader *reader;
+
+    QSortFilterProxyModel *debugProxyModel;
+    QStandardItemModel *debugDataModel;
+
+signals:
+    void insertDebugRow(DebugRow *);
+
+private slots:
+    void clientConnected();
+    void readDebugMsg();
+    void disconnected();
+
+    void filterTextRegExp();
+    void filterColumn();
+    void filterCaseSensitive();
+
+    void appendDebugRow(DebugRow *);
+
+    void saveToFile();
+    void loadFromFile();
+    void clearDebugWindow();
+
+public:
+    void parseReader(QXmlStreamReader *);
+
+};
+
+#endif // MEGADEBUGSERVER_H
