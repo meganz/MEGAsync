c4535e436a819cd5c64b2a72f04f900bc0956dcb
Javier Serrano <js@mega.co.nz>
2014-08-11 11:15:35 +0200

    Version 1.0.29

--- Source/MEGASync/sdk/megaapi.cpp
+++ Source/MEGASync/sdk/megaapi.cpp
@@ -39,7 +39,7 @@ DEALINGS IN THE SOFTWARE.
 #define CLIENT_USER_AGENT "MEGA Android/2.0 BETA"
 #else
 #define CLIENT_KEY "FhMgXbqb"
-#define CLIENT_USER_AGENT "MEGAsync/1.0.27"
+#define CLIENT_USER_AGENT "MEGAsync/1.0.29"
 #endif
 
 #ifdef __APPLE__
