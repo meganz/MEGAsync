2adfdd5d8dd2155ff6d8d14b837af022a580ad72
Victor Teniente <vtmateos@gmail.com>
2014-11-26 02:10:58 +0100

    Beta version 1035.21

--- Source/MEGASync/control/Preferences.cpp
+++ Source/MEGASync/control/Preferences.cpp
@@ -11,9 +11,9 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/1.0.35.20";
+const char Preferences::USER_AGENT[] = "MEGAsync/1.0.35.21";
 const int Preferences::VERSION_CODE = 1035;
-const int Preferences::BUILD_ID = 20;
+const int Preferences::BUILD_ID = 21;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("1.0.36");
 const QString Preferences::TRANSLATION_FOLDER = QString::fromAscii("://translations/");
