a2260bd4c62fcd90f6f600946173ff863fea40eb
Javier Serrano <js@mega.co.nz>
2014-12-15 04:10:22 +0100

    Version 1.0.37 RC
    SDK update

--- Source/MEGASync/control/Preferences.cpp
+++ Source/MEGASync/control/Preferences.cpp
@@ -11,11 +11,11 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/1.0.36";
+const char Preferences::USER_AGENT[] = "MEGAsync/1.0.36h";
 const int Preferences::VERSION_CODE = 1036;
-const int Preferences::BUILD_ID = 1;
+const int Preferences::BUILD_ID = 8;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("1.0.36");
+const QString Preferences::VERSION_STRING = QString::fromAscii("1.0.37");
 const QString Preferences::TRANSLATION_FOLDER = QString::fromAscii("://translations/");
 const QString Preferences::TRANSLATION_PREFIX = QString::fromAscii("MEGASyncStrings_");
 
