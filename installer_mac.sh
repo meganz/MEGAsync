6a1bfa77995cbafb84cf17b6df10faeec69463ad
Victor Teniente <vtmateos@gmail.com>
2015-01-30 19:52:44 +0100

    Update script to compile with Qt 5.4.0

--- installer_mac.sh
+++ installer_mac.sh
@@ -3,14 +3,16 @@
 APP_NAME=MEGAsync
 MOUNTDIR=tmp
 RESOURCES=resourcesDMG
+QTBASE=/QT/qt5/qtbase
 
 rm -rf Release_x64
 mkdir Release_x64
 cd Release_x64
-~/Qt5.3.2/5.3/clang_64/bin/lrelease ../Source/MEGASync/MEGASync.pro
-~/Qt5.3.2/5.3/clang_64/bin/qmake -r ../Source -spec macx-g++ CONFIG+=release CONFIG+=x86_64 -nocache
-make
-~/Qt5.3.2/5.3/clang_64/bin/macdeployqt MEGASync/MEGAsync.app
+$QTBASE/bin/lrelease ../Source/MEGASync/MEGASync.pro
+$QTBASE/bin/qmake -r ../Source -spec macx-g++ CONFIG+=release CONFIG+=x86_64 -nocache
+make -j4
+cp -R MEGASync/MEGAsync.app MEGASync/MEGAsync_orig.app
+$QTBASE/bin/macdeployqt MEGASync/MEGAsync.app
 dsymutil MEGASync/MEGAsync.app/Contents/MacOS/MEGAsync -o MEGAsync.app.dSYM
 strip MEGASync/MEGAsync.app/Contents/MacOS/MEGAsync
 dsymutil MEGALoader/MEGAloader.app/Contents/MacOS/MEGAloader -o MEGAloader.dSYM
@@ -19,54 +21,7 @@ mv MEGASync/MEGAsync.app/Contents/MacOS/MEGAsync MEGASync/MEGAsync.app/Contents/
 mv MEGALoader/MEGAloader.app/Contents/MacOS/MEGAloader MEGASync/MEGAsync.app/Contents/MacOS/MEGAsync
 mv MEGASync/MEGAsync.app ./
 
-cp ~/Qt5.3.2/5.3/clang_64/lib/QtCore.framework/Contents/Info.plist MEGAsync.app/Contents/Frameworks/QtCore.framework/Resources/
-cp ~/Qt5.3.2/5.3/clang_64/lib/QtGui.framework/Contents/Info.plist MEGAsync.app/Contents/Frameworks/QtGui.framework/Resources/
-cp ~/Qt5.3.2/5.3/clang_64/lib/QtMacExtras.framework/Contents/Info.plist MEGAsync.app/Contents/Frameworks/QtMacExtras.framework/Resources/
-cp ~/Qt5.3.2/5.3/clang_64/lib/QtNetwork.framework/Contents/Info.plist MEGAsync.app/Contents/Frameworks/QtNetwork.framework/Resources/
-cp ~/Qt5.3.2/5.3/clang_64/lib/QtPrintSupport.framework/Contents/Info.plist MEGAsync.app/Contents/Frameworks/QtPrintSupport.framework/Resources/
-cp ~/Qt5.3.2/5.3/clang_64/lib/QtWidgets.framework/Contents/Info.plist MEGAsync.app/Contents/Frameworks/QtWidgets.framework/Resources/
-
-#Overwrite resources to manage update processes. Use modified QtNetwork to get a valid signature
-#For signing Installer, use original QtNetwork, otherwise use modified QtNetwork
-#cp -f ~/DesarrolloSW/MEGA_work/overwriteResources/QtNetwork MEGAsync.app/Contents/Frameworks/QtNetwork.framework/Versions/5/
-
 cp -R $APP_NAME.app ${APP_NAME}_unsigned.app
-
-#Save current working directory
-actualDirectory="$PWD"
-
-FRAMEWORK_DIR="$APP_NAME.app/Contents/Frameworks"
-
-# Loop through all frameworks
-FRAMEWORKS=`find "${FRAMEWORK_DIR}" -depth -type d -name "*.framework" | cut -d/ -f4 | sed -e "s/.framework//"`
-
-RESULT=$?
-if [[ $RESULT != 0 ]] ; then
-    exit 1
-fi
-
-echo "Found:"
-echo "${FRAMEWORKS}"
-
-cd $FRAMEWORK_DIR
-
-# Change internal structure of frameworks to fit Mac Guidelines
-for FRAMEWORK in $FRAMEWORKS;
-do
-    cd $FRAMEWORK.framework
-    
-    echo "Setting new Structure"
-
-    ln -s Versions/5/${FRAMEWORK} ${FRAMEWORK}
-    mv Resources/ Versions/5/
-	ln -s Versions/5/Resources Resources
-	ln -s 5 Versions/Current
-    
-    cd ..
-done
-
-#Restore current working directory
-cd "$actualDirectory"
 
 echo "Signing 'APPBUNDLE'"
 codesign --force --verify --verbose --sign "Developer ID Application: Mega Limited" --deep $APP_NAME.app
@@ -107,4 +62,3 @@ echo "DONE"
 
 echo "Checking signature"
 spctl -vvvvvvvvv -a $APP_NAME.app
-
