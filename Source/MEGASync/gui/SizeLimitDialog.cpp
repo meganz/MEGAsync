a5f079d98bd6f6d81b5222832278c15c0c2f95f6
Javier Serrano <js@mega.co.nz>
2015-02-10 20:23:43 +0100

    GUI fixes (Windows)

--- Source/MEGASync/gui/SizeLimitDialog.cpp
+++ Source/MEGASync/gui/SizeLimitDialog.cpp
@@ -7,10 +7,8 @@ SizeLimitDialog::SizeLimitDialog(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    //QRegExp regExp(QString::fromUtf8("\\d+"));
-    QRegExp regExp(QString::fromUtf8("^[0-9]*$"));
-    ui->eUpperThan->setValidator(new QRegExpValidator(regExp, this));
-    ui->eLowerThan->setValidator(new QRegExpValidator(regExp, this));
+    ui->eLowerThan->setMaximum(9999);
+    ui->eUpperThan->setMaximum(9999);
 
     ui->cbExcludeUpperUnit->addItem(QString::fromUtf8("Bytes"));
     ui->cbExcludeUpperUnit->addItem(QString::fromUtf8("KB"));
@@ -25,15 +23,16 @@ SizeLimitDialog::SizeLimitDialog(QWidget *parent) :
     ui->bOK->setDefault(true);
 }
 
-
 bool SizeLimitDialog::upperSizeLimit()
 {
     return ui->cExcludeUpperThan->isChecked();
 }
+
 bool SizeLimitDialog::lowerSizeLimit()
 {
     return ui->cExcludeLowerThan->isChecked();
 }
+
 void SizeLimitDialog::setUpperSizeLimit(bool value)
 {
     ui->cExcludeUpperThan->setChecked(value);
@@ -47,56 +46,47 @@ void SizeLimitDialog::setLowerSizeLimit(bool value)
     ui->eLowerThan->setEnabled(value);
     ui->cbExcludeLowerUnit->setEnabled(value);
 }
+
 void SizeLimitDialog::setUpperSizeLimitValue(long long limit)
 {
-    if(!limit)
-    {
-        ui->eUpperThan->setText(QString::fromUtf8(""));
-    }
-    else
-    {
-        ui->eUpperThan->setText(QString::number(limit));
-    }
+    ui->eUpperThan->setValue(limit);
 }
+
 void SizeLimitDialog::setLowerSizeLimitValue(long long limit)
 {
-    if(!limit)
-    {
-        ui->eLowerThan->setText(QString::fromUtf8(""));
-    }
-    else
-    {
-        ui->eLowerThan->setText(QString::number(limit));
-    }
+    ui->eLowerThan->setValue(limit);
 }
+
 long long SizeLimitDialog::upperSizeLimitValue()
 {
-    bool ok;
-    long long value = ui->eUpperThan->text().trimmed().toLongLong(&ok);
-    return ok ? value : 0;
+    return ui->eUpperThan->value();
 }
+
 long long SizeLimitDialog::lowerSizeLimitValue()
 {
-    bool ok;
-    long long value = ui->eLowerThan->text().trimmed().toLongLong(&ok);
-    return ok ? value : 0;
+    return ui->eLowerThan->value();
 }
+
 void SizeLimitDialog::setUpperSizeLimitUnit(int unit)
 {
     ui->cbExcludeUpperUnit->setCurrentIndex(unit);
 }
+
 void SizeLimitDialog::setLowerSizeLimitUnit(int unit)
 {
     ui->cbExcludeLowerUnit->setCurrentIndex(unit);
 }
+
 int SizeLimitDialog::upperSizeLimitUnit()
 {
     return ui->cbExcludeUpperUnit->currentIndex();
 }
+
 int SizeLimitDialog::lowerSizeLimitUnit()
 {
     return ui->cbExcludeLowerUnit->currentIndex();
 }
+
 void SizeLimitDialog::on_cExcludeUpperThan_clicked()
 {
     if(ui->cExcludeUpperThan->isChecked())
@@ -109,6 +99,7 @@ void SizeLimitDialog::on_cExcludeUpperThan_clicked()
         ui->cbExcludeUpperUnit->setEnabled(false);
     }
 }
+
 void SizeLimitDialog::on_cExcludeLowerThan_clicked()
 {
     if(ui->cExcludeLowerThan->isChecked())
@@ -127,10 +118,10 @@ void SizeLimitDialog::on_bOK_clicked()
     if((upperSizeLimit() && !upperSizeLimitValue()) ||
             (lowerSizeLimit() && !lowerSizeLimitValue()))
     {
-        QMessageBox::information(this, tr("Warning"), tr("Limits can not be zero."), QMessageBox::Ok);
+        QMessageBox::warning(this, tr("Warning"), tr("Limits can not be zero."), QMessageBox::Ok);
         return;
     }
-    this->accept();
+    accept();
 }
 
 void SizeLimitDialog::on_bCancel_clicked()
