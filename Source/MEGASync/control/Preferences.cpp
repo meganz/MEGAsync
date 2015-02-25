cb688055ba69d121473f51a76a3b9266fa1a1ba0
Javier Serrano <js@mega.co.nz>
2015-02-25 20:22:53 +0100

    Version 1999.0

--- Source/MEGASync/control/Preferences.cpp
+++ Source/MEGASync/control/Preferences.cpp
@@ -11,12 +11,12 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/1.0.40.1";
-const int Preferences::VERSION_CODE = 1040;
-const int Preferences::BUILD_ID = 1;
+const char Preferences::USER_AGENT[] = "MEGAsync/2.0.0.0";
+const int Preferences::VERSION_CODE = 1999;
+const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("1.0.40");
-const QString Preferences::SDK_ID = QString::fromAscii("f26a3");
+const QString Preferences::VERSION_STRING = QString::fromAscii("2.0.0");
+const QString Preferences::SDK_ID = QString::fromAscii("6daef");
 const QString Preferences::TRANSLATION_FOLDER = QString::fromAscii("://translations/");
 const QString Preferences::TRANSLATION_PREFIX = QString::fromAscii("MEGASyncStrings_");
 
