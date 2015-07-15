576143559d2f6337ed99cb0441e0c5c682f97e1b
javiserrano <js@mega.co.nz>
2015-07-15 02:47:33 +0200

    New node selector
    New implementation of the node selector using a custom tree model

---
+++ Source/MEGASync/gui/QMegaModel.h
@@ -0,0 +1,41 @@
+#ifndef QMEGAMODEL_H
+#define QMEGAMODEL_H
+
+#include <QAbstractItemModel>
+#include <QList>
+#include <QIcon>
+#include "MegaItem.h"
+#include <megaapi.h>
+
+class QMegaModel : public QAbstractItemModel
+{
+    Q_OBJECT
+public:
+    explicit QMegaModel(mega::MegaApi *megaApi, QObject *parent = 0);
+
+    virtual int	columnCount(const QModelIndex & parent = QModelIndex()) const;
+    virtual QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+    virtual QModelIndex	index(int row, int column, const QModelIndex & parent = QModelIndex()) const;
+    virtual QModelIndex	parent(const QModelIndex & index) const;
+    virtual int	rowCount(const QModelIndex & parent = QModelIndex()) const;
+
+    void setRequiredRights(int requiredRights);
+    void showFiles(bool show);
+    QModelIndex insertNode(mega::MegaNode *node, const QModelIndex &parent);
+    mega::MegaNode *getNode(const QModelIndex &index);
+
+    virtual ~QMegaModel();
+
+protected:
+    mega::MegaApi *megaApi;
+    mega::MegaNode *root;
+    MegaItem *rootItem;
+    QList<MegaItem *> inshareItems;
+    QStringList inshareOwners;
+    QList<mega::MegaNode *> ownNodes;
+    QIcon folderIcon;
+    int requiredRights;
+    bool displayFiles;
+};
+
+#endif // QMEGAMODEL_H
