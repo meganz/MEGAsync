1d449040996452ddca15bea89a900488215bbbb8
Victor Teniente Mateos <vt@mega.co.nz>
2015-08-11 19:25:10 +0200

    Format ChangeLog notes

--- Source/MEGASync/gui/ChangeLogDialog.cpp
+++ Source/MEGASync/gui/ChangeLogDialog.cpp
@@ -7,15 +7,27 @@ ChangeLogDialog::ChangeLogDialog(QString version, QString SDKversion, QString ch
     ui(new Ui::ChangeLogDialog)
 {
     ui->setupUi(this);
-
+    ui->tChangelog->document()->setDocumentMargin(16.0);
     ui->lVersion->setText(version);
     ui->lSDKVersion->setText(QString::fromAscii(" (") + SDKversion + QString::fromAscii(")"));
-    ui->ptChangelog->setPlainText(changeLog);
+    setChangeLogNotes(changeLog);
 }
+
+
 
 ChangeLogDialog::~ChangeLogDialog()
 {
     delete ui;
+}
+
+void ChangeLogDialog::setChangeLogNotes(QString notes)
+{
+    ui->tChangelog->setHtml(QString::fromUtf8("<p style=\"line-height: 119%;\"><span style=\"margin: 16px; font-family: Helvetica; font-size:11px; color: #333333;\">") +
+                             tr("New in this version:") +
+                             QString::fromUtf8("</span></p>") +
+                             QString::fromUtf8("<p style=\" line-height: 146%;\"><span style=\"font-family: Helvetica; font-size:11px; color: #666666;\">") +
+                             notes.replace(QString::fromUtf8("\n"), QString::fromUtf8("<br>")) +
+                             QString::fromUtf8("</span></p>"));
 }
 
 void ChangeLogDialog::on_bTerms_clicked()
