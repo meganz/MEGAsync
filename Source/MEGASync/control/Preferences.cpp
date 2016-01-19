7624e6c6c7573ad2c791b82a1823ea9dedb1646a
Javier Serrano <js@mega.co.nz>
2016-01-19 14:38:26 +0100

    Version 2601.0 for autoupdate

--- Source/MEGASync/control/Preferences.cpp
+++ Source/MEGASync/control/Preferences.cpp
@@ -11,12 +11,12 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/2.6.0.0";
-const int Preferences::VERSION_CODE = 2600;
+const char Preferences::USER_AGENT[] = "MEGAsync/2.6.1.0";
+const int Preferences::VERSION_CODE = 2601;
 const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("2.6.0");
-const QString Preferences::SDK_ID = QString::fromAscii("0a6c1");
+const QString Preferences::VERSION_STRING = QString::fromAscii("2.6.1");
+const QString Preferences::SDK_ID = QString::fromAscii("459bd");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
             "- Streaming from MEGA with a builtin HTTP proxy server\n"
             "- Fixed the automatic detection of the proxy (Win32)\n"
