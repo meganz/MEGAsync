cacc0ffe0435ed94f42a5ec00b7387c65a83f687
Victor Teniente Mateos <vt@mega.co.nz>
2016-02-24 18:18:53 +0100

    Modify OS X system service logic - Upload queue for items outside synced folders - Export and get link for items inside synced folders

--- src/MEGASync/platform/macx/MacXSystemServiceTask.mm
+++ src/MEGASync/platform/macx/MacXSystemServiceTask.mm
@@ -31,11 +31,6 @@ void MacXSystemServiceTask::processItems(QStringList itemsSelected)
         {
             string tmpPath(itemsSelected.at(i).toUtf8().constData());
             MegaNode *node = megaApi->getSyncedNode(&tmpPath);
-            if(!node)
-            {
-                const char *fpLocal = megaApi->getFingerprint(itemsSelected.at(i).toUtf8().constData());
-                node = megaApi->getNodeByFingerprint(fpLocal);
-            }
 
             if(node)
             {
