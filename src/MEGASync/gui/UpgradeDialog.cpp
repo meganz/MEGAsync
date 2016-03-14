9022c9433ae84d801341d0810463f37b4de2835f
Victor Teniente Mateos <vt@mega.co.nz>
2016-03-14 19:31:51 +0100

    Update and fix UI label formats

--- src/MEGASync/gui/UpgradeDialog.cpp
+++ src/MEGASync/gui/UpgradeDialog.cpp
@@ -1,5 +1,6 @@
 #include "UpgradeDialog.h"
 #include "Utilities.h"
+#include "Preferences.h"
 #include "ui_UpgradeDialog.h"
 #include "gui/PlanWidget.h"
 #include <QDebug>
@@ -9,9 +10,17 @@ UpgradeDialog::UpgradeDialog(mega::MegaApi *api, QWidget *parent) :
     ui(new Ui::UpgradeDialog)
 {
     ui->setupUi(this);
+    this->megaApi = api;
+    remainingTime = 0;
 
-    ui->lDescRecommendation->text().replace(QString::fromUtf8("[A]"), QString::fromUtf8("<font color=\"#d90007\"> "))
-                             .replace(QString::fromUtf8("[/A]"), QString::fromUtf8(" </font>"));
+    Preferences *preferences = Preferences::instance();
+    ui->lDescRecommendation->setText(tr("You have utilized %1 of data transfer in the last 6 hours which took you over our current limit."
+                                        " To circumvent this limit, you can [A]upgrade to Pro[/A], "
+                                        "which will give you your own bandwidth package and also ample extra storage space.")
+                                        .arg(preferences->totalBandwidth())
+                                        .replace(QString::fromUtf8("[A]"), QString::fromUtf8("<span style=\" color:#d90007;\">"))
+                                        .replace(QString::fromUtf8("[/A]"), QString::fromUtf8("</span>")));
+
     QHBoxLayout* layout = new QHBoxLayout();
     layout->setContentsMargins(0,0,0,0);
     layout->setSpacing(0);
@@ -19,7 +28,6 @@ UpgradeDialog::UpgradeDialog(mega::MegaApi *api, QWidget *parent) :
     //Add PLAN widgets layout->addWidget()
     ui->wPlans->setLayout(layout);
 
-    remainingTime = 0;
     timer = new QTimer();
     timer->setSingleShot(false);
     connect(timer, SIGNAL(timeout()), this, SLOT(unitTimeElapsed()));
@@ -45,7 +53,7 @@ void UpgradeDialog::unitTimeElapsed()
     if (remainingTime - 1000 >= 0)
     {
         remainingTime -= 1000;
-        ui->lRemainingTime->setText(Utilities::getTimeString(remainingTime));
+        ui->lRemainingTime->setText(tr("Please upgrade to Pro to continue immediately, or wait %1 to continue for free.").arg(Utilities::getTimeString(remainingTime)));
         qDebug() << Utilities::getTimeString(remainingTime);
     }
     else
