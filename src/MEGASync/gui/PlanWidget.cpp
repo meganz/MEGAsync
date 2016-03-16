a4fcd30d8c02f37f32381876bb2950410f604357
Victor Teniente Mateos <vt@mega.co.nz>
2016-03-16 12:34:45 +0100

    Fix hover effects and layout of Plan widgets

--- src/MEGASync/gui/PlanWidget.cpp
+++ src/MEGASync/gui/PlanWidget.cpp
@@ -20,9 +20,9 @@ PlanWidget::PlanWidget(PlanInfo data, QWidget *parent) :
     overlay = new QPushButton(this);
     overlay->setObjectName(QString::fromUtf8("bOverlay"));
     overlay->setStyleSheet(QString::fromAscii(
-                               "QPushButton#bOverlay:hover {border-radius: 3px; border: 1px solid; border-color: rgba(0, 0, 0);} "
+                               "QPushButton#bOverlay:hover {border-radius: 3px; border: 3px solid; border-color: rgba(0, 0, 0);} "
                                "QPushButton#bOverlay {border-radius: 3px; border: 1px solid; border-color: rgba(0, 0, 0, 0.1); background: transparent; border: none;} "
-                               "QPushButton#bOverlay:pressed {border-radius: 3px; border: 1px solid; border-color: rgba(255, 51, 58, 0.8);}"));
+                               "QPushButton#bOverlay:pressed {border-radius: 3px; border: 3px solid; border-color: rgba(255, 51, 58, 0.8);}"));
     overlay->resize(ui->wContainer->minimumSize());
     connect(overlay, SIGNAL(clicked()), this, SLOT(onOverlayClicked()));
 
@@ -65,28 +65,25 @@ void PlanWidget::updatePlanInfo()
     switch (details.level)
     {
         case PRO_LITE:
-            ui->lTip->setVisible(false);
             ui->bcrest->setIcon(QIcon(QString::fromAscii("://images/litecrest.png")));
             ui->bcrest->setIconSize(QSize(58, 58));
             ui->lProPlan->setText(QString::fromUtf8("PRO LITE"));
             ui->lPeriod->setStyleSheet(QString::fromUtf8("color: #ffa500;"));
             break;
         case PRO_I:
-            ui->lTip->setVisible(true);
+            ui->lTip->setText(QString::fromUtf8("popular!"));
             ui->bcrest->setIcon(QIcon(QString::fromAscii("://images/proicrest.png")));
             ui->bcrest->setIconSize(QSize(58, 58));
             ui->lProPlan->setText(QString::fromUtf8("PRO I"));
             ui->lPeriod->setStyleSheet(QString::fromUtf8("color: #ff333a;"));
             break;
         case PRO_II:
-            ui->lTip->setVisible(false);
             ui->bcrest->setIcon(QIcon(QString::fromAscii("://images/proiicrest.png")));
             ui->bcrest->setIconSize(QSize(58, 58));
             ui->lProPlan->setText(QString::fromUtf8("PRO II"));
             ui->lPeriod->setStyleSheet(QString::fromUtf8("color: #ff333a;"));
             break;
         case PRO_III:
-            ui->lTip->setVisible(false);
             ui->bcrest->setIcon(QIcon(QString::fromAscii("://images/proiiicrest.png")));
             ui->bcrest->setIconSize(QSize(58, 58));
             ui->lProPlan->setText(QString::fromUtf8("PRO III"));
