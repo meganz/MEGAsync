ce339eab8556c43e7d3c3be9a2ae079662c922a8
Javier Serrano <js@mega.co.nz>
2016-04-11 13:29:03 +0200

    New changelog and version number

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -11,16 +11,16 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/2.8.0.0";
-const int Preferences::VERSION_CODE = 2800;
+const char Preferences::USER_AGENT[] = "MEGAsync/2.9.0.0";
+const int Preferences::VERSION_CODE = 2900;
 const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("2.8.0");
+const QString Preferences::VERSION_STRING = QString::fromAscii("2.9.0");
 const QString Preferences::SDK_ID = QString::fromAscii("4ed58");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
-            "- Check if public links have been already downloaded\n"
-            "- Better management of communications with the webclient\n"
-            "- Management of bandwidth quotas\n"
+            "- Management of bandwidth quota for PRO users\n"
+            "- New information dialog\n"
+            "- ARM support (Raspberry Pi)\n"
             "- Bug fixes");
 
 const QString Preferences::TRANSLATION_FOLDER = QString::fromAscii("://translations/");
