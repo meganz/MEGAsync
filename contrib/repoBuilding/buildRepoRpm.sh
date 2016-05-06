75aa6ee8939b307745669d550d66e2b658327b3c
polmr <pm@mega.nz>
2016-05-06 10:46:49 +0200

    minor change in buildRepoRpm.sh description

--- contrib/repoBuilding/buildRepoRpm.sh
+++ contrib/repoBuilding/buildRepoRpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##
- # @file contrib/repoBuilding/buildRepoDeb.sh
+ # @file contrib/repoBuilding/buildRepoRpm.sh
  # @brief
  #
  # (c) 2013-2016 by Mega Limited, Auckland, New Zealand
