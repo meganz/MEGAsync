85c50ffcb3f7e7899a83bfe855f641b26ce17546
Javier Serrano <js@mega.co.nz>
2016-05-06 14:17:56 +0200

    Remove BETA from the version string SDK update

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -15,7 +15,7 @@ const char Preferences::USER_AGENT[] = "MEGAsync/2.9.2.1";
 const int Preferences::VERSION_CODE = 2902;
 const int Preferences::BUILD_ID = 1;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("2.9.2_BETA");
+const QString Preferences::VERSION_STRING = QString::fromAscii("2.9.2");
 const QString Preferences::SDK_ID = QString::fromAscii("0436c");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
             "- Save the state and resume transfers\n"
