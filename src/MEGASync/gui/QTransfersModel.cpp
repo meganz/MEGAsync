1e15ddc46501d90483ecc542e7a873615980ffbb
Victor Teniente Mateos <vt@mega.co.nz>
2016-05-10 14:09:09 +0200

    Feed transfer model - Management of transfers within the model - Update of visual transfer data - Insertion/deletion of new transfers

--- src/MEGASync/gui/QTransfersModel.cpp
+++ src/MEGASync/gui/QTransfersModel.cpp
@@ -1,10 +1,17 @@
 #include "QTransfersModel.h"
+#include "MegaApplication.h"
 
 using namespace mega;
 
-QTransfersModel::QTransfersModel(QObject *parent) :
+QTransfersModel::QTransfersModel(MegaTransferList *transfers, int type, QObject *parent) :
     QAbstractItemModel(parent)
 {
+    MegaApi *api =  ((MegaApplication *)qApp)->getMegaApi();
+    this->type = type;
+    delegateListener = new QTMegaTransferListener(api, this);
+    api->addTransferListener(delegateListener);
+
+    setupModelTransfers(transfers);
 }
 
 TransferItem *QTransfersModel::transferFromIndex(const QModelIndex &index) const
@@ -51,14 +58,24 @@ QModelIndex QTransfersModel::index(int row, int column, const QModelIndex &paren
         return QModelIndex();
     }
 
-    return createIndex(row, column, transfers.at(row));
+    return createIndex(row, column, transfers.value(transfersOrder.at(row)));
 }
 
-void QTransfersModel::insertTransfer(TransferItem *transfer, const QModelIndex &parent)
+void QTransfersModel::insertTransfer(MegaTransfer *transfer, const QModelIndex &parent)
 {
     beginInsertRows(QModelIndex(), transfers.size(), transfers.size());
-    transfers.append(transfer);
+    transfers.insert(transfer->getTag(), new TransferItem());
+    transfersOrder.append(transfer->getTag());
     endInsertRows();
+}
+
+void QTransfersModel::removeTransfer(MegaTransfer *transfer, const QModelIndex &parent)
+{
+    int row = transfersOrder.indexOf(transfer->getTag());
+    beginRemoveRows(QModelIndex(), row, row);
+    transfers.remove(transfer->getTag());
+    transfersOrder.removeAt(row);
+    endRemoveRows();
 }
 
 int QTransfersModel::rowCount(const QModelIndex &parent) const
@@ -67,10 +84,78 @@ int QTransfersModel::rowCount(const QModelIndex &parent) const
     {
         return 0;
     }
-    return transfers.size();
+    return transfersOrder.size();
 }
 
 QTransfersModel::~QTransfersModel()
 {
     qDeleteAll(transfers);
+    delete delegateListener;
+}
+
+void QTransfersModel::onTransferStart(MegaApi *api, MegaTransfer *transfer)
+{
+    if (type == TYPE_ALL || transfer->getType() == type)
+    {
+        insertTransfer(transfer, QModelIndex());
+        updateTransferInfo(transfer);
+    }
+
+}
+
+void QTransfersModel::onTransferFinish(MegaApi *api, MegaTransfer *transfer, MegaError *e)
+{
+    if (type == TYPE_ALL || transfer->getType() == type)
+    {
+        if (transfers.contains(transfer->getTag()))
+        {
+            TransferItem *item = transfers.value(transfer->getTag());
+            item->finishTransfer();
+            removeTransfer(transfer, QModelIndex());
+
+            //Update the view
+            emit dataChanged(QModelIndex(), QModelIndex());
+
+        }
+    }
+}
+
+void QTransfersModel::onTransferUpdate(MegaApi *api, MegaTransfer *transfer)
+{
+    if (type == TYPE_ALL || transfer->getType() == type)
+    {
+        if (transfers.contains(transfer->getTag()))
+        {
+            updateTransferInfo(transfer);
+        }
+    }
+
+}
+
+void QTransfersModel::onTransferTemporaryError(MegaApi *api, MegaTransfer *transfer, MegaError *e)
+{
+
+}
+
+void QTransfersModel::setupModelTransfers(MegaTransferList *transfers)
+{
+    for (int i = 0; i < transfers->size(); i++)
+    {
+        MegaTransfer *t = transfers->get(i);
+        insertTransfer(t, QModelIndex());
+    }   
+}
+
+void QTransfersModel::updateTransferInfo(MegaTransfer *transfer)
+{
+    TransferItem *item = transfers.value(transfer->getTag());
+    item->setFileName(QString::fromUtf8(transfer->getFileName()));
+    item->setType(transfer->getType());
+    item->setSpeed(transfer->getSpeed());
+    item->setTotalSize(transfer->getTotalBytes());
+    item->setTransferredBytes(transfer->getTransferredBytes());
+    item->updateTransfer();
+
+    //Update the view
+    emit dataChanged(QModelIndex(), QModelIndex());
 }
--- src/MEGASync/gui/QTransfersModel.h
+++ src/MEGASync/gui/QTransfersModel.h
@@ -4,14 +4,25 @@
 #include <QAbstractItemModel>
 #include "TransferItem.h"
 #include <megaapi.h>
+#include "QTMegaTransferListener.h"
 
-class QTransfersModel : public QAbstractItemModel
+class QTransfersModel : public QAbstractItemModel, public mega::MegaTransferListener
 {
     Q_OBJECT
 public:
-    explicit QTransfersModel(QObject *parent = 0);
 
-    void insertTransfer(TransferItem *transfer, const QModelIndex &parent);
+    enum {
+        TYPE_DOWNLOAD = 0,
+        TYPE_UPLOAD,
+        TYPE_LOCAL_HTTP_DOWNLOAD,
+        TYPE_ALL,
+        TYPE_FINISHED
+    };
+
+    explicit QTransfersModel(mega::MegaTransferList *transfers, int type, QObject *parent = 0);
+
+    void insertTransfer(mega::MegaTransfer *transfer, const QModelIndex &parent);
+    void removeTransfer(mega::MegaTransfer *transfer, const QModelIndex &parent);
     TransferItem *transferFromIndex(const QModelIndex &index) const;
     virtual int columnCount(const QModelIndex & parent = QModelIndex()) const;
     QVariant data(const QModelIndex &index, int role) const;
@@ -20,8 +31,19 @@ public:
     virtual int rowCount(const QModelIndex &parent) const;
     virtual ~QTransfersModel();
 
+    virtual void onTransferStart(mega::MegaApi *api, mega::MegaTransfer *transfer);
+    virtual void onTransferFinish(mega::MegaApi* api, mega::MegaTransfer *transfer, mega::MegaError* e);
+    virtual void onTransferUpdate(mega::MegaApi *api, mega::MegaTransfer *transfer);
+    virtual void onTransferTemporaryError(mega::MegaApi *api, mega::MegaTransfer *transfer, mega::MegaError* e);
+
+private:
+    void setupModelTransfers(mega::MegaTransferList *transfers);
+    void updateTransferInfo(mega::MegaTransfer *transfer);
 protected:
-    QList<TransferItem*> transfers;
+    QMap<int, TransferItem*> transfers;
+    QList<int> transfersOrder;
+    mega::QTMegaTransferListener *delegateListener;
+    int type;
 };
 
 #endif // QTRANSFERSMODEL_H
