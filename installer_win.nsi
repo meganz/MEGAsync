14fcc42900612b1c6eb19fa9aade8679cff15cad
Javier Serrano <js@mega.co.nz>
2016-05-21 19:54:10 +0200

    Release candidate 2904.0

--- installer_win.nsi
+++ installer_win.nsi
@@ -25,10 +25,10 @@ VIAddVersionKey "LegalCopyright" "MEGA Limited 2016"
 VIAddVersionKey "ProductName" "MEGAsync"
 
 ; Version info
-VIProductVersion "2.9.2.0"
-VIAddVersionKey "FileVersion" "2.9.2.0"
-VIAddVersionKey "ProductVersion" "2.9.2.0"
-!define PRODUCT_VERSION "2.9.2_BETA"
+VIProductVersion "2.9.4.0"
+VIAddVersionKey "FileVersion" "2.9.4.0"
+VIAddVersionKey "ProductVersion" "2.9.4.0"
+!define PRODUCT_VERSION "2.9.4"
 
 !define PRODUCT_PUBLISHER "Mega Limited"
 !define PRODUCT_WEB_SITE "http://www.mega.nz"
--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -11,14 +11,15 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/2.9.2.1";
-const int Preferences::VERSION_CODE = 2902;
-const int Preferences::BUILD_ID = 1;
+const char Preferences::USER_AGENT[] = "MEGAsync/2.9.4.0";
+const int Preferences::VERSION_CODE = 2904;
+const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("2.9.2");
-const QString Preferences::SDK_ID = QString::fromAscii("0436c");
+const QString Preferences::VERSION_STRING = QString::fromAscii("2.9.4");
+const QString Preferences::SDK_ID = QString::fromAscii("48ce4");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
             "- Save the state and resume transfers\n"
+            "- Better management of errors during transfers\n"
             "- Compatibility with HDPI displays\n"
             "- Bug fixes");
 
