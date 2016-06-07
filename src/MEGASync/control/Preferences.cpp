a84849396e14e585b48db052de8d9c236b8e24f2
Victor Teniente Mateos <vt@mega.co.nz>
2016-06-07 18:35:05 +0200

    SDK Update bump version

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -16,7 +16,7 @@ const int Preferences::VERSION_CODE = 2906;
 const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("2.9.6");
-const QString Preferences::SDK_ID = QString::fromAscii("78f90");
+const QString Preferences::SDK_ID = QString::fromAscii("64f7b");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
             "- Save the state and resume transfers\n"
             "- Better management of errors during transfers\n"
