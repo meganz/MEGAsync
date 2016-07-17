fb5c1ad39a8d5e69813d2475d537f639bbbd6ae4
Javier Serrano <js@mega.co.nz>
2016-07-17 02:49:29 +0200

    SDK update New build ID

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -11,12 +11,12 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/2.9.7.2";
+const char Preferences::USER_AGENT[] = "MEGAsync/2.9.7.3";
 const int Preferences::VERSION_CODE = 2907;
-const int Preferences::BUILD_ID = 2;
+const int Preferences::BUILD_ID = 3;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("2.9.7");
-const QString Preferences::SDK_ID = QString::fromAscii("65ea4");
+const QString Preferences::SDK_ID = QString::fromAscii("22ac3");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
             "- More efficient upload completion\n"
             "- Creation of new cryptographic keys\n"
