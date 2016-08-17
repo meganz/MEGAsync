1dbdefad30bdb687acea2a0ea8bd4caa2fc84000
Javier Serrano <js@mega.co.nz>
2016-08-17 12:27:31 +0200

    Increase build ID SDK update

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -11,12 +11,12 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/2.9.8.0";
+const char Preferences::USER_AGENT[] = "MEGAsync/2.9.8.2";
 const int Preferences::VERSION_CODE = 2908;
-const int Preferences::BUILD_ID = 0;
+const int Preferences::BUILD_ID = 2;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("2.9.8");
-const QString Preferences::SDK_ID = QString::fromAscii("d7412");
+const QString Preferences::VERSION_STRING = QString::fromAscii("2.9.8.2");
+const QString Preferences::SDK_ID = QString::fromAscii("64f54");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
             "- More efficient upload completion\n"
             "- Creation of new cryptographic keys\n"
