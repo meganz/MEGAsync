9a09140ae0471a07c0ac66a4dacf237b98548032
Victor Teniente Mateos <vt@mega.co.nz>
2016-09-23 10:56:30 +0200

    Enable/disable individual permission type

--- src/MEGASync/gui/PermissionsWidget.cpp
+++ src/MEGASync/gui/PermissionsWidget.cpp
@@ -6,6 +6,7 @@ PermissionsWidget::PermissionsWidget(QWidget *parent) :
     ui(new Ui::PermissionsWidget)
 {
     ui->setupUi(this);
+    configurePermissions(ENABLED_READ | ENABLED_WRITE | ENABLED_EXECUTION);
 }
 
 void PermissionsWidget::setDefaultPermissions(int permissions)
@@ -21,6 +22,36 @@ void PermissionsWidget::updatePermissions()
     (permissionState & Execution) ? ui->cbExecution->setChecked(true) : ui->cbExecution->setChecked(false);
 
     emit onPermissionChanged();
+}
+
+void PermissionsWidget::configurePermissions(unsigned int enabledPermissions)
+{
+    if (enabledPermissions & ENABLED_READ)
+    {
+        ui->cbRead->setEnabled(true);
+    }
+    else
+    {
+        ui->cbRead->setEnabled(false);
+    }
+
+    if (enabledPermissions & ENABLED_WRITE)
+    {
+        ui->cbWrite->setEnabled(true);
+    }
+    else
+    {
+        ui->cbWrite->setEnabled(false);
+    }
+
+    if (enabledPermissions & ENABLED_EXECUTION)
+    {
+        ui->cbExecution->setEnabled(true);
+    }
+    else
+    {
+        ui->cbExecution->setEnabled(false);
+    }
 }
 
 int PermissionsWidget::getCurrentPermissions()
