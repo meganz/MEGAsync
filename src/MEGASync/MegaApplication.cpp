6dcbf609258f3a97f9a5476485abaa7ec1051e6a
Javier Serrano <js@mega.co.nz>
2017-01-22 02:13:22 +0100

    Fixed memory leaks

--- src/MEGASync/MegaApplication.cpp
+++ src/MEGASync/MegaApplication.cpp
@@ -1896,6 +1896,7 @@ void MegaApplication::cleanAll()
     }
 
     closeDialogs();
+    removeAllFinishedTransfers();
 
     delete bwOverquotaDialog;
     bwOverquotaDialog = NULL;
@@ -2903,11 +2904,20 @@ void MegaApplication::toggleLogging()
 
 void MegaApplication::removeFinishedTransfer(int transferTag)
 {
-    finishedTransfers.remove(transferTag);
+    QMap<int, mega::MegaTransfer*>::iterator it = finishedTransfers.find(transferTag);
+    if (it != finishedTransfers.end())
+    {
+        delete it.value();
+        finishedTransfers.erase(it);
+    }
 }
 
 void MegaApplication::removeAllFinishedTransfers()
 {
+    for (QMap<int, mega::MegaTransfer*>::iterator it = finishedTransfers.begin(); it != finishedTransfers.end(); it++)
+    {
+        delete it.value();
+    }
     finishedTransfers.clear();
 }
 
@@ -4818,6 +4828,7 @@ void MegaApplication::onRequestFinish(MegaApi*, MegaRequest *request, MegaError*
         {
             preferences->unlink();
             closeDialogs();
+            removeAllFinishedTransfers();
             delete infoOverQuota;
             infoOverQuota = NULL;
             paused = false;
