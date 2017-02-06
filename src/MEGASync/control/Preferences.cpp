6c1d946f84bc2bcd4a23d2aa42359118beea0332
Javier Serrano <js@mega.co.nz>
2017-02-06 14:06:27 +0100

    SDK update

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -16,7 +16,7 @@ const int Preferences::VERSION_CODE = 3001;
 const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("3.0.1");
-const QString Preferences::SDK_ID = QString::fromAscii("af945");
+const QString Preferences::SDK_ID = QString::fromAscii("bba566");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
             "- Transfer manager\n"
             "- Transfer speed control\n"
