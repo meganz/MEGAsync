d75e8300730406158407f80f8735b0d80564317a
Javier Serrano <js@mega.co.nz>
2017-04-24 21:45:08 +0200

    Updated version code and changelog

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -11,18 +11,16 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/3.0.1.0";
-const int Preferences::VERSION_CODE = 3001;
+const char Preferences::USER_AGENT[] = "MEGAsync/3.0.2.0";
+const int Preferences::VERSION_CODE = 3002;
 const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("3.0.1");
+const QString Preferences::VERSION_STRING = QString::fromAscii("3.1.0");
 const QString Preferences::SDK_ID = QString::fromAscii("bba566");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
-            "- Transfer manager\n"
-            "- Transfer speed control\n"
-            "- Configurable number of connections per transfer\n"
-            "- Improved the transfer speed\n"
-            "- Reduced CPU usage during synchronization\n"
+            "- Better integration with Windows 10\n"
+            "- Updated SSL certificate for communications with the web\n"
+            "- Deprecated OS X versions prior to Mavericks\n"
             "- Bug fixes");
 
 const QString Preferences::TRANSLATION_FOLDER = QString::fromAscii("://translations/");
