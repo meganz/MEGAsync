f39eee73af47d682f9b67f5062ecf59e88e9e28e
Victor Teniente <vt@mega.co.nz>
2018-01-15 20:14:19 +0100

    Update SDK version

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -16,7 +16,7 @@ const int Preferences::VERSION_CODE = 3508;
 const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("3.5.8");
-const QString Preferences::SDK_ID = QString::fromAscii("8bbdd8");
+const QString Preferences::SDK_ID = QString::fromAscii("5c0e09");
 const QString Preferences::CHANGELOG = QString::fromUtf8(
             "- New options related to file versioning\n"
             "- Local backup cleaning scheduler\n"
