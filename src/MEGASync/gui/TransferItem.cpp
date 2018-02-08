b647d64f64745f39a5d1df0bf3ce28092d50e34a
Javier Serrano <js@mega.co.nz>
2018-02-08 17:45:58 +0100

    Use ElideMiddle instead of ElideRight for file names because it's usually useful to see the extension of files with long names and also because it's not uncommon to have several files with long names starting with the same long prefix.

--- src/MEGASync/gui/TransferItem.cpp
+++ src/MEGASync/gui/TransferItem.cpp
@@ -46,9 +46,9 @@ void TransferItem::setFileName(QString fileName)
     this->fileName = fileName;
     QFont f = ui->lTransferName->font();
     QFontMetrics fm = QFontMetrics(f);
-    ui->lTransferNameCompleted->setText(fm.elidedText(fileName, Qt::ElideRight,ui->lTransferNameCompleted->width()));
+    ui->lTransferNameCompleted->setText(fm.elidedText(fileName, Qt::ElideMiddle, ui->lTransferNameCompleted->width()));
     ui->lTransferNameCompleted->setToolTip(fileName);
-    ui->lTransferName->setText(fm.elidedText(fileName, Qt::ElideRight,ui->lTransferName->width()));
+    ui->lTransferName->setText(fm.elidedText(fileName, Qt::ElideMiddle, ui->lTransferName->width()));
     ui->lTransferName->setToolTip(fileName);
 
     QIcon icon;
