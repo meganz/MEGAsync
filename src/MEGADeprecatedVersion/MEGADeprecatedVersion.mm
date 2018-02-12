a99f5de831582af06540b6e6be18bc699d5ae4bd
Victor Teniente <vt@mega.co.nz>
2018-02-12 18:58:14 +0100

    Several fixes: - Memory leak - Modify strings

--- src/MEGADeprecatedVersion/MEGADeprecatedVersion.mm
+++ src/MEGADeprecatedVersion/MEGADeprecatedVersion.mm
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     NSAlert *alert = [[NSAlert alloc] init];
     [alert addButtonWithTitle:@"OK"];
     [alert setMessageText:@"Unsupported macOS Version"];
-    [alert setInformativeText:@"We have detected that you are using and old macOS version that is no longer supported. Please update your system to macOS 10.9+ or download latest compatible installer"];
+    [alert setInformativeText:@"We have detected that you are using and old macOS version that is no longer supported. Please update your system to macOS 10.9+ or download the latest compatible installer of MEGAsync"];
 
     NSTextView *view = [[NSTextView alloc] initWithFrame:NSMakeRect(0,0,200,20)];
     [view setTextContainerInset:NSMakeSize(0.0f, 0.0f)];
@@ -15,8 +15,8 @@ int main(int argc, char *argv[])
     NSDictionary *textAttributes = [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
 
 
-    NSMutableAttributedString* str = [[NSMutableAttributedString alloc] initWithString:@"Download last compatible installer"];
-    [str addAttribute: NSLinkAttributeName value: @"https://mega.nz/sync" range: NSMakeRange(0, str.length)];
+    NSMutableAttributedString* str = [[NSMutableAttributedString alloc] initWithString:@"Click here to download last compatible installer of MEGAsync"];
+    [str addAttribute: NSLinkAttributeName value: @"https://mega.nz/MEGAsyncSetupLegacy.dmg" range: NSMakeRange(0, str.length)];
 
     [view insertText:str];
     [view setEditable:NO];
@@ -26,5 +26,8 @@ int main(int argc, char *argv[])
     [alert setAlertStyle:NSWarningAlertStyle];
     [alert runModal];
     [alert release];
+    [view release];
+    [str release];
+
     return 0;
 }
