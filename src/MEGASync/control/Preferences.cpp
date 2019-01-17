c3bf44de589a96e5258fb886b45d77121aeb62a2
Javier Serrano <js@mega.co.nz>
2019-01-17 14:06:30 +0100

    Bump version

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -11,11 +11,11 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/4.0.0.0";
-const int Preferences::VERSION_CODE = 4000;
+const char Preferences::USER_AGENT[] = "MEGAsync/4.0.1.0";
+const int Preferences::VERSION_CODE = 4001;
 const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("4.0.0");
+const QString Preferences::VERSION_STRING = QString::fromAscii("4.0.1");
 QString Preferences::SDK_ID = QString::fromAscii("975d29");
 const QString Preferences::CHANGELOG = QString::fromUtf8(QT_TR_NOOP(
             "- New design for the main dialog\n"
