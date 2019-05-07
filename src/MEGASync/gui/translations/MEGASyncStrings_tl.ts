edc2775b52bab81018805fbf3a7963cdfc1a0d8b
Victor Teniente <vt@mega.co.nz>
2019-05-07 14:36:43 +0200

    Fix language tag

--- src/MEGASync/gui/translations/MEGASyncStrings_tl.ts
+++ src/MEGASync/gui/translations/MEGASyncStrings_tl.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fil" sourcelanguage="en">
+<TS version="2.1" language="tl" sourcelanguage="en">
 <context>
     <name>AccountDetailsDialog</name>
     <message>
