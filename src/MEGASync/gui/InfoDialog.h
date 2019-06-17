a36a9278fa9116dbb74730f56f26194d5c9b7392
Victor Teniente <vt@mega.co.nz>
2019-06-17 18:45:02 +0200

    Managemente of Add sync option within main dialog

--- src/MEGASync/gui/InfoDialog.h
+++ src/MEGASync/gui/InfoDialog.h
@@ -46,6 +46,7 @@ public:
     void setTransfer(mega::MegaTransfer *transfer);
     void refreshTransferItems();
     void transferFinished(int error);
+    void updateSyncsButton();
     void setIndexing(bool indexing);
     void setWaiting(bool waiting);
     void setOverQuotaMode(bool state);
@@ -86,6 +87,7 @@ public slots:
 private slots:
     void on_bSettings_clicked();
     void on_bUpgrade_clicked();
+    void on_bSyncFolder_clicked();
     void openFolder(QString path);
     void on_bChats_clicked();
     void onOverlayClicked();
@@ -114,6 +116,7 @@ private:
     QWidget *dummy; // Patch to let text input on line edits of GuestWidget
 #endif
 
+    std::unique_ptr<QMenu> syncsMenu;
     QMenu *transferMenu;
     DataUsageMenu *storageUsedMenu;
 
