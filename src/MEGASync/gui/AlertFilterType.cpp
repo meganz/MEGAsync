01546c02cb81d73876b7d5d09d8556b08cc60bf0
Victor Teniente <vt@mega.co.nz>
2019-09-25 13:32:28 +0200

    UI fixes for alert filter widget

--- src/MEGASync/gui/AlertFilterType.cpp
+++ src/MEGASync/gui/AlertFilterType.cpp
@@ -23,28 +23,28 @@ void AlertFilterType::setActualFilter(int type)
         case TYPE_CONTACTS:
             {
                 ui->liconType->setIcon(QIcon(QString::fromUtf8(":/images/contacts.png")).pixmap(6.0, 6.0));
-                ui->liconType->show();
+                ui->wIconType->show();
                 ui->lFilterActive->setText(tr("Contacts"));
             }
             break;
         case TYPE_PAYMENTS:
             {
                 ui->liconType->setIcon(QIcon(QString::fromUtf8(":/images/payments.png")).pixmap(6.0, 6.0));
-                ui->liconType->show();
+                ui->wIconType->show();
                 ui->lFilterActive->setText(tr("Payment"));
             }
             break;
         case TYPE_SHARES:
             {
                 ui->liconType->setIcon(QIcon(QString::fromUtf8(":/images/incoming_share.png")).pixmap(6.0, 6.0));
-                ui->liconType->show();
+                ui->wIconType->show();
                 ui->lFilterActive->setText(tr("Incoming Shares"));
             }
             break;
         case ALL_TYPES:
         default:
             {
-                ui->liconType->hide();
+                ui->wIconType->hide();
                 ui->lFilterActive->setText(tr("All notifications"));
             }
             break;
