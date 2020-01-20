caf1ce336bca4dfc4f3bd6e22ed2819250539332
Matt Weir <mattw@mega.co.nz>
2020-01-20 15:58:39 +1300

    indicate 32/64 bit inside breakpad for windows minidumps

--- src/MEGASync/google_breakpad/client/windows/handler/exception_handler.cc
+++ src/MEGASync/google_breakpad/client/windows/handler/exception_handler.cc
@@ -981,7 +981,7 @@ bool ExceptionHandler::WriteMinidumpWithExceptionForProcess(
   oss << "MEGAprivate ERROR DUMP\n";
   int frame_number=0;
 
-  oss << "Application: " << QApplication::applicationName().toUtf8().constData() << "\n";
+  oss << "Application: " << QApplication::applicationName().toUtf8().constData() << (sizeof(char*) == 4 ? " [32 bit]" : "") << (sizeof(char*) == 8 ? " [64 bit]" : "") << "\n";
   oss << "Version code: " << QString::number(Preferences::VERSION_CODE).toUtf8().constData() <<
          "." << QString::number(Preferences::BUILD_ID).toUtf8().constData() << "\n";
   HMODULE module = GetModuleHandle(NULL);
