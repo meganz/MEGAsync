9d4c7bb309b8a8db3c7f396b6fe880d7414bdd24
Victor Teniente <vt@mega.co.nz>
2020-01-28 18:17:30 +0100

    Bump build id for new RC7

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -13,7 +13,7 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
 const char Preferences::USER_AGENT[] = "MEGAsync/4.3.0.0";
 const int Preferences::VERSION_CODE = 4300;
-const int Preferences::BUILD_ID = 4;
+const int Preferences::BUILD_ID = 7;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("4.3.0");
 QString Preferences::SDK_ID = QString::fromAscii("04dd0fe");
