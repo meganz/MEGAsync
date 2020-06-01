77e900d2471538d401f998f87e893a4c4e7b3844
Victor Teniente <vt@mega.co.nz>
2020-06-01 16:32:04 +0200

    Bump version

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -13,10 +13,10 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
 const char Preferences::USER_AGENT[] = "MEGAsync/4.3.3.0";
 const int Preferences::VERSION_CODE = 4303;
-const int Preferences::BUILD_ID = 1;
+const int Preferences::BUILD_ID = 2;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("4.3.3");
-QString Preferences::SDK_ID = QString::fromAscii("cd8dae2");
+QString Preferences::SDK_ID = QString::fromAscii("29cc212");
 const QString Preferences::CHANGELOG = QString::fromUtf8(QT_TR_NOOP(
     "- New customized message boxes.\n"
     "- Clean retroactive logs when logout.\n"
