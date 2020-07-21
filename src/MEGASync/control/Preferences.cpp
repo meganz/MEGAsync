7da269d9245ffe53f93c06176eea9940063af92c
Pablo Martin <pm@mega.nz>
2020-07-21 20:09:35 +0200

    update sdk to include fixes in filesystem detection, transfer cancellation & scsn sent too early

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -16,7 +16,7 @@ const int Preferences::VERSION_CODE = 4304;
 const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("4.3.4");
-QString Preferences::SDK_ID = QString::fromAscii("c063af3");
+QString Preferences::SDK_ID = QString::fromAscii("754bbfb");
 const QString Preferences::CHANGELOG = QString::fromUtf8(QT_TR_NOOP(
     "- Improve user experience when exceeded transfer allowance.\n"
     "- Improvements in translation.\n"
