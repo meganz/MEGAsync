61131c71e12973e82a4a95ef82ee6cbfd7d2aeea
Victor Teniente <vt@mega.co.nz>
2021-02-11 11:31:10 +0100

    Bump SDK version to release v3.8.2

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -17,7 +17,7 @@ const int Preferences::VERSION_CODE = 4500;
 const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("4.5.0");
-QString Preferences::SDK_ID = QString::fromAscii("");
+QString Preferences::SDK_ID = QString::fromAscii("253f3d6");
 const QString Preferences::CHANGELOG = QString::fromUtf8(QT_TR_NOOP(
     "- New improvements for the management of the syncs.\n"
     "- Improved user experience when syncs are disabled.\n"
