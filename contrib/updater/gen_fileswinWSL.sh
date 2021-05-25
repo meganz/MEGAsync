d41540bc7e8ff5df165f93724acdfc090de5af94
Matt Weir <mattw@mega.co.nz>
2021-05-25 12:01:52 +1200

    Updated updater config fileswin.txt fileswin64.txt for 4501

--- contrib/updater/gen_fileswinWSL.sh
+++ contrib/updater/gen_fileswinWSL.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+#use like this (in debian WSL):
+#mattw@DESKTOP-13186O8:~$ dos2unix /mnt/c/dev/desktop/contrib/updater/gen_fileswinWSL.sh
+#mattw@DESKTOP-13186O8:~$ /mnt/c/dev/desktop/contrib/updater/gen_fileswinWSL.sh /mnt/c/Users/mattw/AppData/Local/MEGAsync
+#then diff with existing version and adjust
+
 echo $1
 (cd $1 && for i in $(find . -type f); do echo "$i;$i;$(sha256sum $i | awk '{print $1}')"; done | sed "s#_CodeSignature#VCodeSignature#g" | sed "s#\.\/##g" | sort -f |  sed "s#VCodeSignature#_CodeSignature#g")
