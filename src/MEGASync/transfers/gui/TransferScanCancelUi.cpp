7c9aecb97a29499fada8033818fccf8b04f37808
Mickaël Da Cruz Guimarães <mg@mega.co.nz>
2022-05-10 13:06:00 -0300

    Fixed styles

--- src/MEGASync/transfers/gui/TransferScanCancelUi.cpp
+++ src/MEGASync/transfers/gui/TransferScanCancelUi.cpp
@@ -56,9 +56,16 @@ void TransferScanCancelUi::onCancelDismissed()
 
 const char* TransferScanCancelUi::getControlStyles()
 {
-    const char* styles = "*[role=\"title\"] {font-size: 18px; font:bold;}"
-                         "*[role=\"details\"] {font-size: 12px;}"
-                         "QPushButton {font-size: 12px; padding: 10px}"
-                         "QPushButton#pProceed {background-color: #00BFA5;}";
+    const char* styles = "*[role=\"title\"] {font-size: 18px; font-weight:500;}"
+                            "*[role=\"details\"] {font-size: 14px;}"
+                            "QPushButton { "
+                            "   font-size: 16px; font-weight:400;"
+                            "   padding-top : 8px; padding-bottom : 8px; padding-left : 15px; padding-right : 15px;"
+                            "   background-color : white;"
+                            "   border-style: solid; border-width: 1px; border-color: #d7d6d5; border-radius: 5px;"
+                            "}"
+                            "QPushButton:pressed { background-color : rgb(238, 238, 236); }"
+                            "QPushButton#pProceed { background-color: rgb(0, 191, 165);}"
+                            "QPushButton#pProceed:pressed { background-color: rgb(0, 179, 155);}";
     return styles;
 }
