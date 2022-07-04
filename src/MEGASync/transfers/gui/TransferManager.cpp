86ad4f75f1fa879634d68a2243a74b3682aacf86
Mauro <mfm@mega.co.nz>
2022-07-04 21:55:22 +0200

    Remove unused variable

--- src/MEGASync/transfers/gui/TransferManager.cpp
+++ src/MEGASync/transfers/gui/TransferManager.cpp
@@ -809,8 +809,6 @@ void TransferManager::on_bPause_toggled()
 
 void TransferManager::pauseResumeTransfers(bool isPaused)
 {
-    auto proxy (mUi->wTransfers->getProxyModel());
-
     mModel->pauseResumeAllTransfers(isPaused);
     onUpdatePauseState(isPaused);
 }
