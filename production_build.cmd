0b43a2d67e5984c485b535376f82a9d910107f26
Matt Weir <mattw@mega.co.nz>
2022-08-31 12:41:27 +1200

    specify targets for production build, just the ones needed in case others fail

--- production_build.cmd
+++ production_build.cmd
@@ -4,10 +4,10 @@ REM first build with
 mkdir build-x64-windows-mega
 cd build-x64-windows-mega
 cmake -G "Visual Studio 16 2019" -A x64 -DMega3rdPartyDir=C:\Users\build\MEGA\build-MEGASync\3rdParty_MSVC2019_20211109\3rdParty_desktop -DMEGA_QT_VERSION=5.12.12 -DVCPKG_TRIPLET=x64-windows-mega -S "..\contrib\cmake" -B .
-cmake --build . --config Release
+cmake --build . --config Release --target MEGAsync --target MEGAUpdater --target MEGAShellExt
 cd ..
 
 mkdir build-x86-windows-mega
 cd build-x86-windows-mega
 cmake -G "Visual Studio 16 2019" -A Win32 -DMega3rdPartyDir=C:\Users\build\MEGA\build-MEGASync\3rdParty_MSVC2019_20211109\3rdParty_desktop  -DMEGA_QT_VERSION=5.12.12 -DVCPKG_TRIPLET=x86-windows-mega -S "..\contrib\cmake" -B .
-cmake --build . --config Release
+cmake --build . --config Release --target MEGAsync --target MEGAUpdater --target MEGAShellExt
