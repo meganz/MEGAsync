8b7228c7521f0d7c109433b3c59c029cb33a1a25
Raphaël Lizé <rl@mega.nz>
2022-09-22 12:25:21 +0200

    Bump version to 4.7.0-backups-preview-13

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -17,8 +17,8 @@ const char Preferences::USER_AGENT[] = "MEGAsync/4.7.0.0";
 const int Preferences::VERSION_CODE = 4700;
 const int Preferences::BUILD_ID = 4;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("4.6.8-backups-preview-12");
-QString Preferences::SDK_ID = QString::fromAscii("badc344");
+const QString Preferences::VERSION_STRING = QString::fromAscii("4.7.0-backups-preview-13");
+QString Preferences::SDK_ID = QString::fromAscii("1c5e526");
 const QString Preferences::CHANGELOG = QString::fromUtf8(QT_TR_NOOP(
 "- Updated strings\n"
 "- Fixed syncs/backups enable when business account expires\n"
