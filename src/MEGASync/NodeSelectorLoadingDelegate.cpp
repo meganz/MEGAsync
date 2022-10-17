bcf69cdd7a23ca6049fa77b519593a3d242092a6
Ekaitz Privado <ep@mega.co.nz>
2022-10-17 09:46:53 +0200

    missing loading scene files

---
+++ src/MEGASync/NodeSelectorLoadingDelegate.cpp
@@ -0,0 +1,19 @@
+#include "NodeSelectorLoadingDelegate.h"
+#include "ui_NodeSelectorLoadingDelegate.h"
+
+NodeSelectorLoadingDelegate::NodeSelectorLoadingDelegate(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::NodeSelectorLoadingDelegate)
+{
+    ui->setupUi(this);
+}
+
+NodeSelectorLoadingDelegate::~NodeSelectorLoadingDelegate()
+{
+    delete ui;
+}
+
+QSize NodeSelectorLoadingDelegate::widgetSize()
+{
+    return QSize(100, 20);
+}
