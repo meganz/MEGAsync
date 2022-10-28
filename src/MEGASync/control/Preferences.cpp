a0e30e0fdef7d64f0e1dba1337b9f9f9022aaec3
Victor Teniente <vt@mega.co.nz>
2022-10-28 11:50:55 +0200

    Bump version v4.7.2 RC2 and SDK v4.5.0

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -15,10 +15,10 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
 const char Preferences::USER_AGENT[] = "MEGAsync/4.7.2.0";
 const int Preferences::VERSION_CODE = 4702;
-const int Preferences::BUILD_ID = 0;
+const int Preferences::BUILD_ID = 1;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("4.7.2");
-QString Preferences::SDK_ID = QString::fromAscii("d96a911");
+QString Preferences::SDK_ID = QString::fromAscii("2cad2e5e");
 const QString Preferences::CHANGELOG = QString::fromUtf8(QT_TR_NOOP(
 "- Added new Apple Silicon native support.\n"
 "- Added a new feature to stop sleep mode if there are active transfers.\n"
