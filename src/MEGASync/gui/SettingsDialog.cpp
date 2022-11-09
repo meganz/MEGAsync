7f9d23fcc656dafc1bb6481b379aef6abd2f51d0
Mauro Ferreiro <mfm@mega.co.nz>
2022-11-09 15:38:23 +0100

    Fixes: 1) SNC-3056: The FullName request is valid as soon as both requests have been received 2) SNC-3058: Increase the height of the Settings Dialog to fit the new Sleep Mode

--- src/MEGASync/gui/SettingsDialog.cpp
+++ src/MEGASync/gui/SettingsDialog.cpp
@@ -41,7 +41,7 @@ using namespace mega;
 #ifdef Q_OS_MACOS
 //Const values used for macOS Settings dialog resize animation
 constexpr auto SETTING_ANIMATION_PAGE_TIMEOUT{150};//ms
-constexpr auto SETTING_ANIMATION_GENERAL_TAB_HEIGHT{657};
+constexpr auto SETTING_ANIMATION_GENERAL_TAB_HEIGHT{677};
 constexpr auto SETTING_ANIMATION_ACCOUNT_TAB_HEIGHT{295};//px height
 constexpr auto SETTING_ANIMATION_SYNCS_TAB_HEIGHT{529};
 constexpr auto SETTING_ANIMATION_FOLDERS_TAB_HEIGHT{525};
