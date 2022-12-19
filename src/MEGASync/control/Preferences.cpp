2cc8b75823213fc313cf65902663fc13273406b2
Victor Teniente <vt@mega.co.nz>
2022-12-19 11:54:03 +0100

    Update SDK to v4.9.0a

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -17,7 +17,7 @@ const int Preferences::VERSION_CODE = 4805;
 const int Preferences::BUILD_ID = 2;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("4.8.5");
-QString Preferences::SDK_ID = QString::fromAscii("fbf8634");
+QString Preferences::SDK_ID = QString::fromAscii("36c9059");
 const QString Preferences::CHANGELOG = QString::fromUtf8(QT_TR_NOOP(
 "- Detected crashes on Windows, Linux, and masOS fixed.\n"
 "- Less device memory will be used by accounts with large amounts of data stored.\n"
