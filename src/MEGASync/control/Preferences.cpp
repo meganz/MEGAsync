546eb92f0c79c89026e4cb0a8189c45f59c9ed75
Victor Teniente <vt@mega.co.nz>
2023-01-12 11:44:52 +0100

    Bump version v4.8.6 and SDK to v4.9.0b

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -12,12 +12,12 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 #endif
 
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
-const char Preferences::USER_AGENT[] = "MEGAsync/4.8.5.0";
-const int Preferences::VERSION_CODE = 4805;
-const int Preferences::BUILD_ID = 3;
+const char Preferences::USER_AGENT[] = "MEGAsync/4.8.6.0";
+const int Preferences::VERSION_CODE = 4806;
+const int Preferences::BUILD_ID = 0;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
-const QString Preferences::VERSION_STRING = QString::fromAscii("4.8.5");
-QString Preferences::SDK_ID = QString::fromAscii("36c9059");
+const QString Preferences::VERSION_STRING = QString::fromAscii("4.8.6");
+QString Preferences::SDK_ID = QString::fromAscii("016322a");
 const QString Preferences::CHANGELOG = QString::fromUtf8(QT_TR_NOOP(
 "- Detected crashes on Windows, Linux, and masOS fixed.\n"
 "- Less device memory will be used by accounts with large amounts of data stored.\n"
