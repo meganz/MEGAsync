76749b89faf1f5f43a68218e797ff046056e11ad
Victor Teniente <vt@mega.co.nz>
2023-01-20 10:31:37 +0100

    Bump version v4.8.6 RC3 for linux (only)

--- src/MEGASync/control/Preferences.cpp
+++ src/MEGASync/control/Preferences.cpp
@@ -14,7 +14,7 @@ extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
 const char Preferences::CLIENT_KEY[] = "FhMgXbqb";
 const char Preferences::USER_AGENT[] = "MEGAsync/4.8.6.0";
 const int Preferences::VERSION_CODE = 4806;
-const int Preferences::BUILD_ID = 1;
+const int Preferences::BUILD_ID = 2;
 // Do not change the location of VERSION_STRING, create_tarball.sh parses this file
 const QString Preferences::VERSION_STRING = QString::fromAscii("4.8.6");
 QString Preferences::SDK_ID = QString::fromAscii("016322a");
