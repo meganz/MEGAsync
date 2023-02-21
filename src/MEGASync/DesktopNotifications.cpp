9c41be38700f958f665cb08faafded9dad4b765e
Raphaël Lizé <rl@mega.nz>
2023-02-21 19:54:42 +0100

    New Share notification: show Show button to send the user to webclient to sort things out

--- src/MEGASync/DesktopNotifications.cpp
+++ src/MEGASync/DesktopNotifications.cpp
@@ -401,14 +401,16 @@ void DesktopNotifications::notifySharedUpdate(mega::MegaUserAlert *alert, const 
     }
     notification->setTitle(sharedFolderName);
     notification->setText(message);
-    if(node && node->isNodeKeyDecrypted())
+    if(node)
     {
         notification->setData(QString::fromUtf8(node->getBase64Handle()));
         const auto megaApi = static_cast<MegaApplication*>(qApp)->getMegaApi();
         const auto fullAccess = megaApi->getAccess(node.get()) >= mega::MegaShare::ACCESS_FULL;
         QStringList actions (tr("Show"));
 
-        if(type == NEW_SHARE && fullAccess)
+        if(type == NEW_SHARE
+           && fullAccess
+           && node->isNodeKeyDecrypted())
         {
 #ifndef __APPLE__
             // Apple do not support multi option with notifications. Just provide default one.
