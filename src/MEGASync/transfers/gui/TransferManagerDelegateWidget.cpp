73ed017a8118c871145ecd58c97b0a0c46319d38
Mauro Ferreiro Monteagudo <mfm@mega.co.nz>
2023-05-19 21:33:10 +1200

    SNC-3491. Add new date time format for failed transfers

--- src/MEGASync/transfers/gui/TransferManagerDelegateWidget.cpp
+++ src/MEGASync/transfers/gui/TransferManagerDelegateWidget.cpp
@@ -211,7 +211,9 @@ void TransferManagerDelegateWidget::updateTransferState()
                 showTPauseResume = false;
             }
 
-            timeString = getData()->getFormattedFinishedTime();
+            auto dateTime = getData()->getFinishedDateTime();
+            timeString = MegaSyncApp->getFormattedDateByCurrentLanguage(dateTime, QLocale::FormatType::ShortFormat);
+
             timeTooltip = getData()->getFullFormattedFinishedTime();
             speedString = QString::fromUtf8("…");
 
