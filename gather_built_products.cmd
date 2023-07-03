593149d6fa2d1ac0b2371b6163ea5ea7ed7b4b7a
Matt Weir <mattw@mega.co.nz>
2023-07-03 15:36:39 +1200

    gather step adjustments for building SRW windows installers
    Make deleting old folders optional.  Usually unnecessary and might cause hassles getting all the files again if they are in nonstandard locations
    Provide easy options to use the local 3rdParty, just un-REM some lines
    Fix environment variable names

--- gather_built_products.cmd
+++ gather_built_products.cmd
@@ -1,19 +1,23 @@
-IF EXIST built64 (
+IF "%MEGA_CLEAN_BUILT_FOLDERS%" == "true" (
+  REM Clean up any previous leftovers
+  IF EXIST built64 (
     rmdir /s /q built64
-)
-IF EXIST sign64 (
+  )
+  IF EXIST sign64 (
     rmdir /s /q sign64
+  )
 )
 
 IF [%MEGA_VCPKGPATH%]==[] (
-	SET VCPKGPATH=C:\Users\build\MEGA\build-MEGASync\3rdParty_MSVC2019_20221029\3rdParty_desktop
+    SET MEGA_VCPKGPATH=C:\Users\build\MEGA\build-MEGASync\3rdParty_MSVC2019_20221029\3rdParty_desktop
+    rem SET MEGA_VCPKGPATH=%CD%\..\3rdparty_desktop
 )
 
 IF [%MEGA_THIRD_PARTY_DLL_DIR%]==[] (
-	SET THIRD_PARTY_DLL_DIR=bin_dlls_signed
+    SET MEGA_THIRD_PARTY_DLL_DIR=bin_dlls_signed
+    rem SET MEGA_THIRD_PARTY_DLL_DIR=bin
 )
 
-REM Clean up any previous leftovers
 mkdir built64
 mkdir sign64
 
@@ -31,12 +35,13 @@ IF "%MEGA_SKIP_32_BIT_BUILD%" == "true" (
 	GOTO :EOF
 )
 
-REM Clean up any previous leftovers
-IF EXIST built32 (
-	rmdir /s /q built32
-)
-IF EXIST sign32 (
-	rmdir /s /q sign32
+IF "%MEGA_CLEAN_BUILT_FOLDERS%" == "true" (
+  IF EXIST built32 (
+    rmdir /s /q built32
+  )
+  IF EXIST sign32 (
+    rmdir /s /q sign32
+  )
 )
 
 mkdir built32
