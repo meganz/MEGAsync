782484731592b337805df520b333c706e4769bba
Victor Teniente <vt@mega.co.nz>
2023-07-07 10:37:26 +0200

    Bump version v4.9.6 and SDK v4.25.0

--- src/MEGASync/control/Version.h
+++ src/MEGASync/control/Version.h
@@ -1,13 +1,13 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             4, 9, 5, 0
-#define VER_FILEVERSION_CODE        4905
-#define VER_PRODUCTVERSION          4, 9, 5, 0
+#define VER_FILEVERSION             4, 9, 6, 0
+#define VER_FILEVERSION_CODE        4906
+#define VER_PRODUCTVERSION          4, 9, 6, 0
 // Update scrips relying on this value if you move it
-#define VER_PRODUCTVERSION_STR      "4.9.5.0\0"
+#define VER_PRODUCTVERSION_STR      "4.9.6.0\0"
 
-#define VER_BUILD_ID                4
+#define VER_BUILD_ID                0
 
 #define VER_COMPANYNAME_STR         "Mega Limited\0"
 #define VER_FILEDESCRIPTION_STR     "MEGAsync\0"
@@ -17,7 +17,7 @@
 #define VER_ORIGINALFILENAME_STR    "MEGAsync.exe\0"
 #define VER_PRODUCTNAME_STR         "MEGAsync\0"
 
-#define VER_SDK_ID                  "0f65318"
+#define VER_SDK_ID                  "d45eacf"
 
 // Update scrips relying on this value if you move it
 // Format: 1 item by line, starting from line following the #define
@@ -28,7 +28,7 @@
 //"- item n\n"
 //)
 #define VER_CHANGES_NOTES  QT_TRANSLATE_NOOP("Preferences",                                        \
-"- We've enhanced system notifications.\n"                                                         \
+"- New management of display of error messages.\n"                                                         \
 "- We've enhanced the UI.\n"                                                                       \
 "- We've fixed the detected crashes on Windows, Linux, and macOS.\n"                               \
 )
