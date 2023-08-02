c3452be2e3223d7af49afabc8f3ed07e6c68fcef
Raphaël Lizé <rl@mega.nz>
2023-08-02 16:51:13 +0200

    Windows Qt deploy script: adapt windeployqt options

--- deploy_qt.cmd
+++ deploy_qt.cmd
@@ -7,7 +7,8 @@ IF EXIST %MEGA_QT_DEPLOY_DIR% (
 
 mkdir %MEGA_QT_DEPLOY_DIR%
 C:\Qt\5.12.12\msvc2017_64\bin\windeployqt.exe --no-translations --no-compiler-runtime ^
- --no-opengl-sw --no-system-d3d-compiler ^
+ --no-opengl-sw --no-system-d3d-compiler --no-angle --no-webkit2 --no-qmltooling ^
+ --no-patchqt --no-designercomponents ^
  --dir %MEGA_QT_DEPLOY_DIR% ^
  --qmldir src\MEGASync\gui\onboarding\qml\ ^
  --qmldir src\MEGASync\gui\qml\ ^
@@ -16,16 +17,7 @@ C:\Qt\5.12.12\msvc2017_64\bin\windeployqt.exe --no-translations --no-compiler-ru
  build-x64-windows-mega\Release\MEGAShellExt.dll
 
 rmdir /s /q %MEGA_QT_DEPLOY_DIR%\bearer
-rmdir /s /q %MEGA_QT_DEPLOY_DIR%\qmltooling
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\iconengines
-rmdir /s /q %MEGA_QT_DEPLOY_DIR%\Qt
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtGraphicalEffects
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQml
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQuick
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQuick.2
 rmdir /s /q %MEGA_QT_DEPLOY_DIR%\scenegraph
-del %MEGA_QT_DEPLOY_DIR%\libEGL.dll
-del %MEGA_QT_DEPLOY_DIR%\libGLESV2.dll
 
 IF "%MEGA_SKIP_32_BIT_BUILD%" == "true" (
 	GOTO :EOF
@@ -40,7 +32,8 @@ IF EXIST %MEGA_QT_DEPLOY_DIR% (
 
 mkdir %MEGA_QT_DEPLOY_DIR%
 C:\Qt\5.12.12\msvc2017\bin\windeployqt.exe --no-translations --no-compiler-runtime ^
- --no-opengl-sw --no-system-d3d-compiler ^
+ --no-opengl-sw --no-system-d3d-compiler --no-angle --no-webkit2 --no-qmltooling ^
+ --no-patchqt --no-designercomponents ^
  --dir %MEGA_QT_DEPLOY_DIR% ^
  --qmldir src\MEGASync\gui\onboarding\qml ^
  --qmldir src\MEGASync\gui\qml\ ^
@@ -49,13 +42,4 @@ C:\Qt\5.12.12\msvc2017\bin\windeployqt.exe --no-translations --no-compiler-runti
  build-x86-windows-mega\Release\MEGAupdater.exe ^
 
 rmdir /s /q %MEGA_QT_DEPLOY_DIR%\bearer
-rmdir /s /q %MEGA_QT_DEPLOY_DIR%\qmltooling
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\iconengines
-rmdir /s /q %MEGA_QT_DEPLOY_DIR%\Qt
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtGraphicalEffects
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQml
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQuick
-::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQuick.2
 rmdir /s /q %MEGA_QT_DEPLOY_DIR%\scenegraph
-del %MEGA_QT_DEPLOY_DIR%\libEGL.dll
-del %MEGA_QT_DEPLOY_DIR%\libGLESV2.dll
