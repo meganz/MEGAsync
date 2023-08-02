af7eae1d039f7390c58237ae38453d7f27130df2
Raphaël Lizé <rl@mega.nz>
2023-08-02 13:51:17 +0200

    Windows Qt deploy script: remove unused files

--- deploy_qt.cmd
+++ deploy_qt.cmd
@@ -16,6 +16,16 @@ C:\Qt\5.12.12\msvc2017_64\bin\windeployqt.exe --no-translations --no-compiler-ru
  build-x64-windows-mega\Release\MEGAShellExt.dll
 
 rmdir /s /q %MEGA_QT_DEPLOY_DIR%\bearer
+rmdir /s /q %MEGA_QT_DEPLOY_DIR%\qmltooling
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\iconengines
+rmdir /s /q %MEGA_QT_DEPLOY_DIR%\Qt
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtGraphicalEffects
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQml
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQuick
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQuick.2
+rmdir /s /q %MEGA_QT_DEPLOY_DIR%\scenegraph
+del %MEGA_QT_DEPLOY_DIR%\libEGL.dll
+del %MEGA_QT_DEPLOY_DIR%\libGLESV2.dll
 
 IF "%MEGA_SKIP_32_BIT_BUILD%" == "true" (
 	GOTO :EOF
@@ -39,3 +49,13 @@ C:\Qt\5.12.12\msvc2017\bin\windeployqt.exe --no-translations --no-compiler-runti
  build-x86-windows-mega\Release\MEGAupdater.exe ^
 
 rmdir /s /q %MEGA_QT_DEPLOY_DIR%\bearer
+rmdir /s /q %MEGA_QT_DEPLOY_DIR%\qmltooling
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\iconengines
+rmdir /s /q %MEGA_QT_DEPLOY_DIR%\Qt
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtGraphicalEffects
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQml
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQuick
+::rmdir /s /q %MEGA_QT_DEPLOY_DIR%\QtQuick.2
+rmdir /s /q %MEGA_QT_DEPLOY_DIR%\scenegraph
+del %MEGA_QT_DEPLOY_DIR%\libEGL.dll
+del %MEGA_QT_DEPLOY_DIR%\libGLESV2.dll
