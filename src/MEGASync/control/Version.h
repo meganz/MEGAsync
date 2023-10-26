9d9fab8db170cbde19479f42a5a8ec06530dd516
Victor Teniente <vt@mega.co.nz>
2023-10-26 11:09:11 +0200

    Bump version v4.11.0 and SDK v4.29.1b

--- src/MEGASync/control/Version.h
+++ src/MEGASync/control/Version.h
@@ -1,13 +1,13 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             4, 10, 0, 0
-#define VER_FILEVERSION_CODE        41000
-#define VER_PRODUCTVERSION          4, 10, 0, 0
+#define VER_FILEVERSION             4, 11, 0, 0
+#define VER_FILEVERSION_CODE        41100
+#define VER_PRODUCTVERSION          4, 11, 0, 0
 // Update scrips relying on this value if you move it
-#define VER_PRODUCTVERSION_STR      "4.10.0.0\0"
+#define VER_PRODUCTVERSION_STR      "4.11.0.0\0"
 
-#define VER_BUILD_ID                2
+#define VER_BUILD_ID                0
 
 #define VER_COMPANYNAME_STR         "Mega Limited\0"
 #define VER_FILEDESCRIPTION_STR     "MEGAsync\0"
@@ -17,7 +17,7 @@
 #define VER_ORIGINALFILENAME_STR    "MEGAsync.exe\0"
 #define VER_PRODUCTNAME_STR         "MEGAsync\0"
 
-#define VER_SDK_ID                  "91d14ae"
+#define VER_SDK_ID                  "6d4c102"
 
 // Update scrips relying on this value if you move it
 // Format: 1 item by line, starting from line following the #define
