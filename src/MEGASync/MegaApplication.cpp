00388e53325e2cf2e87fd30d31f3cc1c61ac8499
Omar Atef <oa@mega.co.nz>
2023-10-26 21:48:50 +1300

    SNC-3987. Lots of deprecated method in Qt 5.15 (Release)

--- src/MEGASync/MegaApplication.cpp
+++ src/MEGASync/MegaApplication.cpp
@@ -2199,10 +2199,7 @@ void MegaApplication::cleanAll()
     CrashHandler::instance()->Disable();
 #endif
 
-    qInstallMsgHandler(0);
-#if QT_VERSION >= 0x050000
     qInstallMessageHandler(0);
-#endif
 
     periodicTasksTimer->stop();
     networkCheckTimer->stop();
@@ -2275,7 +2272,7 @@ void MegaApplication::cleanAll()
     {
 #ifndef __APPLE__
         QString app = QString::fromUtf8("\"%1\"").arg(MegaApplication::applicationFilePath());
-        QProcess::startDetached(app);
+        QProcess::startDetached(app, {});
 #else
         QString app = MegaApplication::applicationDirPath();
         QString launchCommand = QString::fromUtf8("open");
@@ -2554,7 +2551,7 @@ void MegaApplication::calculateInfoDialogCoordinates(QDialog *dialog, int *posx,
         }
     #else
         #ifdef WIN32
-            QRect totalGeometry = QApplication::desktop()->screenGeometry();
+            QRect totalGeometry = QGuiApplication::primaryScreen()->geometry();
             APPBARDATA pabd;
             pabd.cbSize = sizeof(APPBARDATA);
             pabd.hWnd = FindWindow(L"Shell_TrayWnd", NULL);
@@ -3138,7 +3135,7 @@ QString MegaApplication::obfuscateAddress(const QHostAddress &ipAddress)
 
 QString MegaApplication::obfuscateIpv4Address(const QHostAddress &ipAddress)
 {
-    const QStringList addressParts = ipAddress.toString().split(QChar::fromAscii('.'));
+    const QStringList addressParts = ipAddress.toString().split(QChar::fromLatin1('.'));
     if (addressParts.size() == 4)
     {
         auto itAddressPart = addressParts.begin()+2;
@@ -3167,8 +3164,8 @@ QStringList MegaApplication::explodeIpv6(const QHostAddress &ipAddress)
     auto ipv6 = ipAddress.toIPv6Address();
     for (int i=0; i<8; ++i) {
         const int baseI = i*2;
-        addressParts.push_back(QString::fromUtf8("%1%2").arg(ipv6[baseI], 0, 16, QChar::fromAscii('0'))
-                                                        .arg(ipv6[baseI+1], 0, 16, QChar::fromAscii('0')));
+        addressParts.push_back(QString::fromUtf8("%1%2").arg(ipv6[baseI], 0, 16, QChar::fromLatin1('0'))
+                                                        .arg(ipv6[baseI+1], 0, 16, QChar::fromLatin1('0')));
     }
     return addressParts;
 }
@@ -3436,7 +3433,7 @@ void MegaApplication::processUpgradeSecurityEvent()
     {
         message.append(QLatin1String("<br><br>"));
         message.append(tr("You are currently sharing the following folder: %1", "", outSharesStrings.size())
-                  .arg(outSharesStrings.toList().join(QLatin1String(", "))));
+                  .arg(outSharesStrings.values().join(QLatin1String(", "))));
     }
 
     QMegaMessageBox::MessageBoxInfo msgInfo;
@@ -3650,7 +3647,7 @@ void MegaApplication::cleanLocalCaches(bool all)
                             continue;
                         }
 
-                        QDateTime creationTime(cacheFolder.created());
+                        QDateTime creationTime(cacheFolder.birthTime());
                         if (all || (creationTime.isValid() && creationTime.daysTo(QDateTime::currentDateTime()) > timeLimitDays) )
                         {
                             Utilities::removeRecursively(cacheFolder.canonicalFilePath());
