c76fe144055a56e5f8e736b477b01ab962b09e22
Raphaël Lizé <rl@mega.nz>
2023-11-22 14:25:24 +0100

    Symlinks processing: +comments

--- src/MEGASync/main.cpp
+++ src/MEGASync/main.cpp
@@ -198,6 +198,11 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationVersion(QString::number(Preferences::VERSION_CODE));
 
     Platform::create();
+
+    // This call is responsible for rebuilding the Qt symlinks in platforms where it applies.
+    // This needs to be done when starting the first time after an update.
+    // For this to work, the call needs to know the version of the app BEFORE updating,
+    // so needs to be made before the file megasync.version is updated.
     Platform::getInstance()->processSymLinks();
 
     if ((argc == 2) && !strcmp("/uninstall", argv[1]))
@@ -555,6 +560,8 @@ int main(int argc, char *argv[])
         #endif
     }
 
+    // The megasync.version file update needs to be done AFTER Platform::getInstance()->processSymLinks(),
+    // because it needs the old version number.
     QString appVersionPath = dataDir.filePath(QString::fromUtf8("megasync.version"));
     QFile fappVersionPath(appVersionPath);
     if (fappVersionPath.open(QIODevice::WriteOnly))
