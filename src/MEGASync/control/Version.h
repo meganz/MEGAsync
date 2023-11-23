a02b8bbc0e11a0d7e9f5a7fcdcf668a2def510ea
Mauro Ferreiro <mfm@mega.co.nz>
2023-11-23 08:34:58 +0100

    Bump version 5.1.0 RC2

--- src/MEGASync/control/Version.h
+++ src/MEGASync/control/Version.h
@@ -7,7 +7,7 @@
 // Update scripts relying on this value if you move it
 #define VER_PRODUCTVERSION_STR      "5.1.0.0\0"
 
-#define VER_BUILD_ID                0
+#define VER_BUILD_ID                1
 
 #define VER_COMPANYNAME_STR         "Mega Limited\0"
 #define VER_FILEDESCRIPTION_STR     "MEGAsync\0"
@@ -17,7 +17,7 @@
 #define VER_ORIGINALFILENAME_STR    "MEGAsync.exe\0"
 #define VER_PRODUCTNAME_STR         "MEGAsync\0"
 
-#define VER_SDK_ID                  "ff437ae"
+#define VER_SDK_ID                  "66a65a8"
 
 // Update scrips relying on this value if you move it
 // Format: 1 item by line, starting from line following the #define
