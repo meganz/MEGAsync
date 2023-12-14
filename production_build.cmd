b2cc7e7b367fd89f55d8d1632b7e1575141f3066
Raphaël Lizé <rl@mega.nz>
2023-12-14 13:46:23 +0100

    Windows build scripts: use -j%MEGA_CORES%

--- production_build.cmd
+++ production_build.cmd
@@ -10,6 +10,10 @@ IF [%MEGA_QTPATH%]==[] (
 	)
 )
 
+IF [%MEGA_CORES%]==[] (
+	FOR /f "tokens=2 delims==" %%f IN ('wmic cpu get NumberOfLogicalProcessors /value ^| find "="') DO SET MEGA_CORES=%%f
+)
+
 REM Clean up any previous leftovers
 IF EXIST build-x64-windows-mega (
     rmdir /s /q build-x64-windows-mega
@@ -18,7 +22,7 @@ IF EXIST build-x64-windows-mega (
 mkdir build-x64-windows-mega
 cd build-x64-windows-mega
 cmake -G "Visual Studio 16 2019" -A x64 -DMega3rdPartyDir=%MEGA_VCPKGPATH% -DQT_DIR=%MEGA_QTPATH% -DCMAKE_PREFIX_PATH=%MEGA_QTPATH% -DVCPKG_TRIPLET=x64-windows-mega -S "..\contrib\cmake" -B .
-cmake --build . --config Release --target MEGAsync --target MEGAUpdater --target MEGAShellExt
+cmake --build . --config Release --target MEGAsync --target MEGAUpdater --target MEGAShellExt -j%MEGA_CORES%
 cd ..
 
 IF "%MEGA_SKIP_32_BIT_BUILD%" == "true" (
@@ -33,5 +37,5 @@ IF EXIST build-x86-windows-mega (
 mkdir build-x86-windows-mega
 cd build-x86-windows-mega
 cmake -G "Visual Studio 16 2019" -A Win32 -DMega3rdPartyDir=%MEGA_VCPKGPATH% -DQT_DIR=%MEGA_QTPATH% -DCMAKE_PREFIX_PATH=%MEGA_QTPATH%\..\x86 -DVCPKG_TRIPLET=x86-windows-mega -S "..\contrib\cmake" -B .
-cmake --build . --config Release --target MEGAsync --target MEGAUpdater --target MEGAShellExt
+cmake --build . --config Release --target MEGAsync --target MEGAUpdater --target MEGAShellExt -j%MEGA_CORES%
 cd ..
