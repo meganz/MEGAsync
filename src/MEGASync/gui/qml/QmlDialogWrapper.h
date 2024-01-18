a7cff4fd14ba1238609548aae76893d954349438
Ekaitz Privado <ep@mega.co.nz>
2024-01-18 12:01:41 +0100

    fix windows compilation

--- src/MEGASync/gui/qml/QmlDialogWrapper.h
+++ src/MEGASync/gui/qml/QmlDialogWrapper.h
@@ -142,7 +142,7 @@ public:
             * Errors will be printed respecting the original format (with links to source qml that fails).
             * All errors will be printed, using qDebug() some errors were hidden.
             */
-            mega::MegaApi::log(mega::MegaApi::LOG_LEVEL_ERROR, qmlComponent.errorString().toStdString().c_str());
+            ::mega::MegaApi::log(::mega::MegaApi::LOG_LEVEL_ERROR, qmlComponent.errorString().toStdString().c_str());
         }
     }
 
