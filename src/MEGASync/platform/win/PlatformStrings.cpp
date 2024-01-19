95029077df939c3432c45ab3c89b386471411f7f
Omar Atef <oa@mega.co.nz>
2024-01-20 02:45:59 +1300

    Remove unused SRW strings

--- src/MEGASync/platform/win/PlatformStrings.cpp
+++ src/MEGASync/platform/win/PlatformStrings.cpp
@@ -5,11 +5,6 @@ QString PlatformStrings::movedFileToBin()
     return tr("Moved to OS Recycle Bin");
 }
 
-QString PlatformStrings::bin()
-{
-    return tr("Recycle Bin");
-}
-
 QString PlatformStrings::fileExplorer()
 {
     return tr("Show in Explorer");
