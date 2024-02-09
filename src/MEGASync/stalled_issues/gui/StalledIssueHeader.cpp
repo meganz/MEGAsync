678989ebccc119988db8bd8cc5a7dcaea4c5e5d7
Omar Atef <oa@mega.co.nz>
2024-02-09 15:29:21 +0200

    Fix for label text format

--- src/MEGASync/stalled_issues/gui/StalledIssueHeader.cpp
+++ src/MEGASync/stalled_issues/gui/StalledIssueHeader.cpp
@@ -35,6 +35,7 @@ StalledIssueHeader::StalledIssueHeader(QWidget *parent) :
     ui->setupUi(this);
     connect(ui->multipleActionButton, &QPushButton::clicked, this, &StalledIssueHeader::onMultipleActionClicked);
     ui->fileNameTitle->setTextFormat(Qt::TextFormat::AutoText);
+    ui->errorDescriptionText->setTextFormat(Qt::TextFormat::AutoText);
     QSizePolicy sp_retain = ui->arrow->sizePolicy();
     sp_retain.setRetainSizeWhenHidden(true);
     ui->arrow->setSizePolicy(sp_retain);
