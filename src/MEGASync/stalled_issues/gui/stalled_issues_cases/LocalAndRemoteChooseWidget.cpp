374546e389cbb5804d6fdf207562031143e4447e
Mauro Ferreiro <mfm@mega.co.nz>
2024-04-30 10:01:58 +0200

    Added missing new classes

---
+++ src/MEGASync/stalled_issues/gui/stalled_issues_cases/LocalAndRemoteChooseWidget.cpp
@@ -0,0 +1,224 @@
+#include "LocalAndRemoteChooseWidget.h"
+#include "ui_StalledIssueChooseWidget.h"
+#include "StalledIssuesModel.h"
+
+#include <platform/PlatformStrings.h>
+
+///// Choose titles
+///
+///
+//BASE CLASS
+LocalAndRemoteStalledIssueBaseChooseWidget::LocalAndRemoteStalledIssueBaseChooseWidget(QWidget *parent) :
+    StalledIssueChooseWidget(parent)
+{
+}
+
+LocalAndRemoteStalledIssueBaseChooseWidget::~LocalAndRemoteStalledIssueBaseChooseWidget()
+{
+}
+
+void LocalAndRemoteStalledIssueBaseChooseWidget::updateUi(StalledIssueDataPtr data,
+    LocalOrRemoteUserMustChooseStalledIssue::ChosenSide side)
+{
+    auto fileName = data->getFileName();
+    ui->chooseTitle->showIcon();
+
+    ui->name->setTitle(fileName);
+
+    mega::MegaHandle handle(mega::INVALID_HANDLE);
+    if(data->isCloud())
+    {
+        auto cloudData = data->convert<CloudStalledIssueData>();
+        if(cloudData)
+        {
+            handle = cloudData->getPathHandle();
+        }
+    }
+
+    ui->name->setInfo(data->getNativeFilePath(), handle);
+    ui->name->setIsFile(data->isFile());
+    ui->name->setIsCloud(data->isCloud());
+    ui->name->showIcon();
+
+    ui->path->show();
+    ui->path->updateUi(data);
+
+    if((side != LocalOrRemoteUserMustChooseStalledIssue::ChosenSide::NONE) !=
+        ui->chooseTitle->isSolved())
+    {
+        ui->chooseTitle->hideActionButton(StalledIssueChooseWidget::BUTTON_ID);
+
+        QIcon icon;
+        if(side == LocalOrRemoteUserMustChooseStalledIssue::ChosenSide::REMOTE)
+        {
+            if(data->isCloud())
+            {
+                icon.addFile(QString::fromUtf8(":/images/StalledIssues/check_default.png"));
+                ui->chooseTitle->setMessage(tr("Chosen"), icon.pixmap(24,24));
+            }
+            else
+            {
+                icon.addFile(QString::fromUtf8(":/images/StalledIssues/remove_default.png"));
+                ui->chooseTitle->setMessage(solvedString(), icon.pixmap(24,24));
+            }
+        }
+        else if(side == LocalOrRemoteUserMustChooseStalledIssue::ChosenSide::LOCAL)
+        {
+            if(data->isCloud())
+            {
+                icon.addFile(QString::fromUtf8(":/images/StalledIssues/remove_default.png"));
+                ui->chooseTitle->setMessage(solvedString(), icon.pixmap(24,24));
+            }
+            else
+            {
+                icon.addFile(QString::fromUtf8(":/images/StalledIssues/check_default.png"));
+                ui->chooseTitle->setMessage(tr("Local file is being uploaded"), icon.pixmap(16,16));
+            }
+        }
+        else if(side == LocalOrRemoteUserMustChooseStalledIssue::ChosenSide::BOTH)
+        {
+            icon.addFile(QString::fromUtf8(":/images/StalledIssues/check_default.png"));
+
+            if(data->isCloud())
+            {
+                auto cloudData = data->convert<CloudStalledIssueData>();
+                auto node(cloudData->getNode(true));
+                if(node)
+                {
+                    ui->chooseTitle->setMessage(tr("Renamed to %1").arg(QString::fromUtf8(node->getName())), icon.pixmap(24,24));
+                }
+            }
+            else
+            {
+                ui->chooseTitle->setMessage(QString());
+            }
+        }
+    }
+
+    mData = data;
+}
+
+const StalledIssueDataPtr &LocalAndRemoteStalledIssueBaseChooseWidget::data()
+{
+    return mData;
+}
+
+//LOCAL
+QString LocalStalledIssueChooseWidget::solvedString() const
+{
+    return PlatformStrings::movedFileToBin();
+}
+
+void LocalStalledIssueChooseWidget::updateUi(LocalStalledIssueDataPtr localData,
+    LocalOrRemoteUserMustChooseStalledIssue::ChosenSide side)
+{
+    updateExtraInfo(localData);
+    ui->chooseTitle->setTitle(tr("Local Copy"));
+    ui->chooseTitle->setIsCloud(false);
+
+    LocalAndRemoteStalledIssueBaseChooseWidget::updateUi(localData,side);
+
+    if(side != LocalOrRemoteUserMustChooseStalledIssue::ChosenSide::NONE)
+    {
+        setChosen(side == LocalOrRemoteUserMustChooseStalledIssue::ChosenSide::LOCAL);
+    }
+}
+
+void LocalStalledIssueChooseWidget::onRawInfoToggled()
+{
+    updateExtraInfo(mData->convert<LocalStalledIssueData>());
+}
+
+void LocalStalledIssueChooseWidget::updateExtraInfo(LocalStalledIssueDataPtr localData)
+{
+    localData->getFileFolderAttributes()->requestModifiedTime(this, [this](const QDateTime& time){
+        ui->name->updateLastTimeModified(time);
+    });
+
+#ifndef Q_OS_LINUX
+    localData->getFileFolderAttributes()->requestCreatedTime(this, [this](const QDateTime& time){
+        ui->name->updateCreatedTime(time);
+    });
+#endif
+
+    localData->getFileFolderAttributes()->requestSize(this, [this](qint64 size){
+        ui->name->updateSize(size);
+    });
+
+    if(MegaSyncApp->getStalledIssuesModel()->isRawInfoVisible())
+    {
+        localData->getFileFolderAttributes()->requestCRC(this, [this](const QString& fp)
+            {
+                ui->name->updateCRC(fp);
+            });
+    }
+    else
+    {
+        ui->name->updateCRC(QString());
+    }
+}
+
+//CLOUD
+QString CloudStalledIssueChooseWidget::solvedString() const
+{
+    return tr("Moved to MEGA Bin");
+}
+
+void CloudStalledIssueChooseWidget::updateUi(CloudStalledIssueDataPtr cloudData,
+    LocalOrRemoteUserMustChooseStalledIssue::ChosenSide side)
+{
+    updateExtraInfo(cloudData);
+    ui->chooseTitle->setTitle(tr("Remote Copy"));
+    ui->chooseTitle->setIsCloud(true);
+
+    LocalAndRemoteStalledIssueBaseChooseWidget::updateUi(cloudData, side);
+
+    if(side != LocalOrRemoteUserMustChooseStalledIssue::ChosenSide::NONE)
+    {
+        setChosen(side == LocalOrRemoteUserMustChooseStalledIssue::ChosenSide::REMOTE);
+    }
+}
+
+void CloudStalledIssueChooseWidget::onRawInfoToggled()
+{
+    updateExtraInfo(mData->convert<CloudStalledIssueData>());
+}
+
+void CloudStalledIssueChooseWidget::updateExtraInfo(CloudStalledIssueDataPtr cloudData)
+{
+    auto node = cloudData->getNode();
+    if(node)
+    {
+        cloudData->getFileFolderAttributes()->requestModifiedTime(this, [this](const QDateTime& time){
+            ui->name->updateLastTimeModified(time);
+        });
+
+        cloudData->getFileFolderAttributes()->requestCreatedTime(this, [this](const QDateTime& time){
+            ui->name->updateCreatedTime(time);
+        });
+
+        cloudData->getFileFolderAttributes()->requestSize(this, [this](qint64 size){
+            ui->name->updateSize(size);
+        });
+
+        if(MegaSyncApp->getStalledIssuesModel()->isRawInfoVisible())
+        {
+            cloudData->getFileFolderAttributes()->requestCRC(this, [this](const QString& fp)
+                {
+                    ui->name->updateCRC(fp);
+                });
+        }
+        else
+        {
+            ui->name->updateCRC(QString());
+        }
+
+        cloudData->getFileFolderAttributes()->requestVersions(this, [this](int versions){
+            ui->name->updateVersionsCount(versions);
+        });
+
+        cloudData->getFileFolderAttributes()->requestUser(this, MegaSyncApp->getMegaApi()->getMyUserHandleBinary(), [this](QString user, bool show){
+            ui->name->updateUser(user, show);
+        });
+    }
+}
---
+++ src/MEGASync/stalled_issues/gui/stalled_issues_cases/MoveOrRenameCannotOccurChooseWidget.cpp
@@ -0,0 +1,114 @@
+#include "MoveOrRenameCannotOccurChooseWidget.h"
+
+#include "ui_StalledIssueChooseWidget.h"
+#include "syncs/control/SyncController.h"
+
+//BASE CLASS
+MoveOrRenameCannotOccurChooseWidget::MoveOrRenameCannotOccurChooseWidget(QWidget *parent) :
+    mSide(MoveOrRenameCannotOccurIssue::ChosenSide::NONE),
+    StalledIssueChooseWidget(parent)
+{
+}
+
+MoveOrRenameCannotOccurChooseWidget::~MoveOrRenameCannotOccurChooseWidget()
+{
+}
+
+void MoveOrRenameCannotOccurChooseWidget::updateUi(
+    std::shared_ptr<const MoveOrRenameCannotOccurIssue> issue)
+{
+    ui->chooseTitle->showIcon();
+
+    ui->name->setHyperLinkMode();
+    ui->name->setIsFile(issue->isFile());
+    ui->name->setInfo(QString(), mega::INVALID_HANDLE);
+    ui->name->showIcon();
+
+    ui->pathContainer->hide();
+
+    auto side(issue->getChosenSide());
+
+    if(side != MoveOrRenameCannotOccurIssue::ChosenSide::NONE)
+    {
+        ui->chooseTitle->hideActionButton(StalledIssueChooseWidget::BUTTON_ID);
+
+        QIcon icon;
+        if(side == mSide)
+        {
+            icon.addFile(QString::fromUtf8(":/images/StalledIssues/check_default.png"));
+            ui->chooseTitle->setMessage(tr("Chosen"), icon.pixmap(16, 16));
+        }
+        else
+        {
+            icon.addFile(QString::fromUtf8(":/images/StalledIssues/rotate-ccw.png"));
+            ui->chooseTitle->setMessage(tr("Changes Undone"), icon.pixmap(16, 16));
+        }
+    }
+
+    update();
+
+    mData = issue;
+}
+
+
+//LOCAL CLASS
+LocalMoveOrRenameCannotOccurChooseWidget::LocalMoveOrRenameCannotOccurChooseWidget(QWidget *parent) :
+    MoveOrRenameCannotOccurChooseWidget(parent)
+{
+    mSide = MoveOrRenameCannotOccurIssue::ChosenSide::LOCAL;
+}
+
+LocalMoveOrRenameCannotOccurChooseWidget::~LocalMoveOrRenameCannotOccurChooseWidget() {}
+
+void LocalMoveOrRenameCannotOccurChooseWidget::updateUi(
+    std::shared_ptr<const MoveOrRenameCannotOccurIssue> issue)
+{
+    MoveOrRenameCannotOccurChooseWidget::updateUi(issue);
+    ui->chooseTitle->setHTML(tr("Local"));
+    ui->name->setIsCloud(false);
+
+    if(issue->isSolved())
+    {
+        setChosen(issue->getChosenSide() == MoveOrRenameCannotOccurIssue::ChosenSide::LOCAL);
+    }
+
+    std::unique_ptr<mega::MegaSync> sync(MegaSyncApp->getMegaApi()->getSyncByBackupId(mData->syncIds().first()));
+    if(sync)
+    {
+        QString pathStr(QString::fromUtf8(sync->getLocalFolder()));
+        ui->name->setHTML(pathStr);
+        ui->name->setInfo(pathStr, mega::INVALID_HANDLE);
+    }
+}
+
+//REMOTE CLASS
+RemoteMoveOrRenameCannotOccurChooseWidget::RemoteMoveOrRenameCannotOccurChooseWidget(QWidget *parent) :
+    MoveOrRenameCannotOccurChooseWidget(parent)
+{
+     mSide = MoveOrRenameCannotOccurIssue::ChosenSide::REMOTE;
+}
+
+RemoteMoveOrRenameCannotOccurChooseWidget::~RemoteMoveOrRenameCannotOccurChooseWidget() {}
+
+void RemoteMoveOrRenameCannotOccurChooseWidget::updateUi(
+    std::shared_ptr<const MoveOrRenameCannotOccurIssue> issue)
+{
+    MoveOrRenameCannotOccurChooseWidget::updateUi(issue);
+    ui->chooseTitle->setHTML(tr("Remote"));
+    ui->name->setIsCloud(true);
+
+    if(issue->isSolved())
+    {
+        setChosen(issue->getChosenSide() == MoveOrRenameCannotOccurIssue::ChosenSide::REMOTE);
+    }
+
+    std::unique_ptr<mega::MegaSync> sync(MegaSyncApp->getMegaApi()->getSyncByBackupId(mData->syncIds().first()));
+    if(sync)
+    {
+        std::unique_ptr<mega::MegaNode> remoteFolder(MegaSyncApp->getMegaApi()->getNodeByHandle(sync->getMegaHandle()));
+        std::unique_ptr<const char[]> path(MegaSyncApp->getMegaApi()->getNodePath(remoteFolder.get()));
+        QString pathStr(QString::fromUtf8(path.get()));
+        ui->name->setInfo(pathStr, mega::INVALID_HANDLE);
+        ui->name->setHTML(QString::fromUtf8(path.get()));
+    }
+}
