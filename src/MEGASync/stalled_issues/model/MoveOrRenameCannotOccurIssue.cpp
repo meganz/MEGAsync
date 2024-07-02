7242ae236487ed90879eb21fa529483774f87cb8
Mauro Ferreiro <mfm@mega.co.nz>
2024-07-02 10:24:13 +0200

    Compilation error fixed

--- src/MEGASync/stalled_issues/model/MoveOrRenameCannotOccurIssue.cpp
+++ src/MEGASync/stalled_issues/model/MoveOrRenameCannotOccurIssue.cpp
@@ -59,6 +59,11 @@ bool MoveOrRenameCannotOccurIssue::isAutoSolvable() const
     return false;
 }
 
+bool MoveOrRenameCannotOccurIssue::refreshListAfterSolving() const
+{
+    return true;
+}
+
 void MoveOrRenameCannotOccurIssue::solveIssue(MoveOrRenameIssueChosenSide side)
 {
     if(!syncIds().isEmpty())
