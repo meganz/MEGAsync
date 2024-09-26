7f86c20c3a13ddee55521dda93d22df0588e17f7
Fernando García <fga@mega.co.nz>
2024-09-26 13:04:00 +0200

    Use UpsellController in the InfoDialog to calculate the min plan.
    Also, move code from Utilities to UpsellController.

--- src/MEGASync/gui/upsell/UpsellController.h
+++ src/MEGASync/gui/upsell/UpsellController.h
@@ -50,6 +50,8 @@ public:
     void setViewMode(UpsellPlans::ViewMode mode);
     void setTransferFinishTime(long long finishTime);
 
+    QString getMinProPlanNeeded(long long usedStorage) const;
+
 public slots:
     void onBilledPeriodChanged();
 
