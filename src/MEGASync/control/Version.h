f35a224087360e5c1915739578c1290f4c95a005
Raphaël Lizé <rl@mega.nz>
2024-11-06 17:25:34 +0100

    Bump desktop app version and sdk version

--- src/MEGASync/control/Version.h
+++ src/MEGASync/control/Version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION 5, 6, 0, 0
-#define VER_FILEVERSION_CODE 50600
-#define VER_PRODUCTVERSION 5, 6, 0, 0
+#define VER_FILEVERSION 5, 7, 0, 0
+#define VER_FILEVERSION_CODE 50700
+#define VER_PRODUCTVERSION 5, 7, 0, 0
 // Update scripts relying on this value if you move it
-#define VER_PRODUCTVERSION_STR "5.6.0.0\0"
+#define VER_PRODUCTVERSION_STR "5.7.0.0\0"
 
 #define VER_BUILD_ID                0
 
@@ -17,7 +17,8 @@
 #define VER_ORIGINALFILENAME_STR    "MEGAsync.exe\0"
 #define VER_PRODUCTNAME_STR         "MEGAsync\0"
 
-#define VER_SDK_ID "6073288"
+/* SDK commit hash, 7 chars */
+#define VER_SDK_ID "a014e4c"
 
 /* Update scrips relying on this value if you move it
 Format: 1 item by line, starting from line following the #define
