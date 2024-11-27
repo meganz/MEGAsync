7caa2548ce89dc53a568ef2cc0f493c8e1ed926e
Raphaël Lizé <rl@mega.nz>
2024-11-27 17:39:25 +0100

    Linux: Qt build: adapt to build Qt 5.15.16

--- contrib/build_qt/linux/qt/build_minimum.sh
+++ contrib/build_qt/linux/qt/build_minimum.sh
@@ -51,15 +51,11 @@ wget -c "https://download.qt.io/official_releases/qt/$mainver/$ver/single/$SRC_A
 # Get patches
 mkdir -p patches
 for p in \
-CVE-2023-32763-qtbase-5.15.diff \
-CVE-2023-34410-qtbase-5.15.diff \
-CVE-2023-37369-qtbase-5.15.diff \
-CVE-2023-38197-qtbase-5.15.diff \
-CVE-2023-43114-5.15.patch \
 0001-CVE-2023-51714-qtbase-5.15.diff \
 0002-CVE-2023-51714-qtbase-5.15.diff \
 CVE-2024-25580-qtbase-5.15.diff \
-CVE-2024-36048-qtnetworkauth-5.15.diff
+CVE-2024-36048-qtnetworkauth-5.15.diff \
+CVE-2024-39936-qtbase-5.15.patch
 do
   wget -c "https://download.qt.io/official_releases/qt/$mainver/$p" -O "patches/$p"
 done
@@ -67,16 +63,13 @@ done
 # Verify checksums
 [ $download_only -eq 1 ] && exit 0
 echo "CHECKSUM ... "
-if ! echo fdd3a4f197d2c800ee0085c721f4bef60951cbda9e9c46e525d1412f74264ed7 "$SRC_ARCHIVE" | sha256sum -c - \
-|| ! echo e8acecaea4d0e78408fc7049c44c7fb44804d459e7c7f700e08fe0b7d4052256 patches/CVE-2023-32763-qtbase-5.15.diff | sha256sum -c - \
-|| ! echo 01917eae0587b72f1969c303289a26bc1a148fea2eef7a64e05e6e86c1dcc178 patches/CVE-2023-34410-qtbase-5.15.diff | sha256sum -c - \
-|| ! echo 279c520ec96994d2b684ddd47a4672a6fdfc7ac49a9e0bdb719db1e058d9e5c0 patches/CVE-2023-37369-qtbase-5.15.diff | sha256sum -c - \
-|| ! echo 382c10ec8f42e2a34ac645dc4f57cd6b717abe6a3807b7d5d9312938f91ce3dc patches/CVE-2023-38197-qtbase-5.15.diff | sha256sum -c - \
-|| ! echo 14cc26aa465ec9a5cac6f6b4c91b2f802b12a8134a6ab897a45449c418ca98c1 patches/CVE-2023-43114-5.15.patch | sha256sum -c - \
+if ! echo efa99827027782974356aceff8a52bd3d2a8a93a54dd0db4cca41b5e35f1041c "$SRC_ARCHIVE" | sha256sum -c - \
 || ! echo 2129058a5e24d98ee80a776c49a58c2671e06c338dffa7fc0154e82eef96c9d4 patches/0001-CVE-2023-51714-qtbase-5.15.diff | sha256sum -c - \
 || ! echo 99d5d32527e767d6ab081ee090d92e0b11f27702619a4af8966b711db4f23e42 patches/0002-CVE-2023-51714-qtbase-5.15.diff | sha256sum -c - \
 || ! echo 7cc9bf74f696de8ec5386bb80ce7a2fed5aa3870ac0e2c7db4628621c5c1a731 patches/CVE-2024-25580-qtbase-5.15.diff | sha256sum -c - \
-|| ! echo e5d385d636b5241b59ac16c4a75359e21e510506b26839a4e2033891245f33f9 patches/CVE-2024-36048-qtnetworkauth-5.15.diff | sha256sum -c -
+|| ! echo e5d385d636b5241b59ac16c4a75359e21e510506b26839a4e2033891245f33f9 patches/CVE-2024-36048-qtnetworkauth-5.15.diff | sha256sum -c - \
+|| ! echo 2cc23afba9d7e48f8faf8664b4c0324a9ac31a4191da3f18bd0accac5c7704de patches/CVE-2024-39936-qtbase-5.15.patch | sha256sum -c -
+
 
 then
 exit 1
@@ -85,19 +78,15 @@ fi
 # Extract sources
 echo "EXTRACT SOURCES ... "
 [ -d src ] && rm -r src;
-mkdir -p src && tar xvf "$SRC_ARCHIVE" -C src --strip-components=1
+mkdir -p src && tar xf "$SRC_ARCHIVE" -C src --strip-components=1
 
 # Apply patches
 echo "APPLY PATCHES ... "
 for p in \
-CVE-2023-32763-qtbase-5.15.diff \
-CVE-2023-34410-qtbase-5.15.diff \
-CVE-2023-37369-qtbase-5.15.diff \
-CVE-2023-38197-qtbase-5.15.diff \
-CVE-2023-43114-5.15.patch \
 0001-CVE-2023-51714-qtbase-5.15.diff \
 0002-CVE-2023-51714-qtbase-5.15.diff \
-CVE-2024-25580-qtbase-5.15.diff 
+CVE-2024-25580-qtbase-5.15.diff \
+CVE-2024-39936-qtbase-5.15.patch
 do
   echo "Applying patch: patches/$p"
   if ! patch -f --verbose -p1 -d src/qtbase < patches/$p ; then
