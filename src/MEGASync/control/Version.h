73baf5eb957c5b66d01c604b65aa925c78a207dd
Raphaël Lizé <rl@mega.nz>
2025-03-06 18:41:20 +0100

    Bump version to 5.10.0 RC1, sdk to v9.0.0-rc.1

--- src/MEGASync/control/Version.h
+++ src/MEGASync/control/Version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION 5, 9, 0, 0
-#define VER_FILEVERSION_CODE 50900
-#define VER_PRODUCTVERSION 5, 9, 0, 0
+#define VER_FILEVERSION 5, 10, 0, 0
+#define VER_FILEVERSION_CODE 51000
+#define VER_PRODUCTVERSION 5, 10, 0, 0
 // Update scripts relying on this value if you move it
-#define VER_PRODUCTVERSION_STR "5.9.0.0\0"
+#define VER_PRODUCTVERSION_STR "5.10.0.0\0"
 
 #define VER_BUILD_ID                0
 
@@ -18,7 +18,7 @@
 #define VER_PRODUCTNAME_STR         "MEGAsync\0"
 
 /* SDK commit hash, 7 chars */
-#define VER_SDK_ID "0743fb7"
+#define VER_SDK_ID "df916c5"
 
 /* Update scrips relying on this value if you move it
 Format: 1 item by line, starting from line following the #define
