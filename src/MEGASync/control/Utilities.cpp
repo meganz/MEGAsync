c5c21482f509adb14baa21a46ade963c4290fd47
Omar Atef <oa@mega.co.nz>
2025-03-12 14:25:26 +0200

    Sync's context menu: Open in MEGA doesn't open the correct URL

--- src/MEGASync/control/Utilities.cpp
+++ src/MEGASync/control/Utilities.cpp
@@ -1497,7 +1497,7 @@ void Utilities::openInMega(MegaHandle handle)
             std::unique_ptr<char[]> h (node->getBase64Handle());
             if(h)
             {
-                openUrl(QUrl(QLatin1String("mega://#fm//device-centre/") + deviceID +
+                openUrl(QUrl(QLatin1String("mega://#fm/device-centre/") + deviceID +
                              QString::fromUtf8("/") + QString::fromLatin1(h.get())));
             }
         }
