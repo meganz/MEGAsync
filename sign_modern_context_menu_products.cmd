a02295a1871f36ad518384e86104c7103e3b5d61
Mauro Ferreiro <mfm@mega.co.nz>
2025-03-14 07:02:45 +0100

    Cleaning file

--- sign_modern_context_menu_products.cmd
+++ sign_modern_context_menu_products.cmd
@@ -1,9 +1,21 @@
 @echo off
 
+:: To sign the .msix file, it is mandatory that the publisher of the AppxManifest.xml file
+:: is identical to the subject of the certificate to be used for signing.
+:: To achieve this, the AppxManifest.xml is created with a publisher taken from an environment variable called MEGA_CERTIFICATE_PUBLISHER.
+:: Be careful because that publisher must comply with the xml rules, so you must escape the characters not allowed in xml.
+:: For example, quotation marks (") will be replaced by quot;
+
+:: Working on My certificate store
+set CERT_STORE=My
+
+:: Certificate subject (Change it in case you have a different certificate subject)
+set CERT_SUBJECT="Mega Limited"
+
 :: Check if the first argument is "--help"
 if "%1"=="--help" (
-    echo Usage: sign_context_menu_products.cmd [option] [arguments]
-    echo The default certificate subject name is "Mega Limited"
+    echo Usage: sign_modern_context_menu_products.cmd [option] [arguments]
+    echo The default certificate subject name is %CERT_SUBJECT%
     echo.
     echo Options:
     echo   --help       Display this help message
@@ -11,7 +23,6 @@ if "%1"=="--help" (
     echo.
     exit /b
 )
-
 
 :: Check if the directory path argument was provided
 if "%1"=="--path" (
@@ -25,19 +36,15 @@ if "%DIR_PATH%"=="" (
         exit /b 1
     )
 
-echo The files path is %DIR_PATH%
-
-:: Define the store (can be "My" for Current User, "CA" for Certificate Authorities, etc.)
-set CERT_STORE=My
+echo Working directory: %DIR_PATH%
 
 :: Check if the certificate is installed in the specified store
-echo Checking if the certificate with subject containing "Mega Limited" is installed in the "%CERT_STORE%" store...
+echo Checking if the certificate with subject containing %CERT_SUBJECT% is installed in the "%CERT_STORE%" store...
 
-::certutil -store "%CERT_STORE%" | findstr /i "*%CERT_SUBJECT%*" >nul
-for /f "delims=" %%i in ('powershell -Command "$cert = Get-ChildItem -Path Cert:\CurrentUser\My| Where-Object { $_.Subject -match 'Mega Limited' }; if ($cert) { $cert.Thumbprint } else { 'Certificate not found' }"') do set Thumbprint=%%i
+for /f "delims=" %%i in ('powershell -Command "$cert = Get-ChildItem -Path Cert:\CurrentUser\My| Where-Object { $_.Subject -match '%CERT_SUBJECT%' }; if ($cert) { $cert.Thumbprint } else { 'Certificate not found' }"') do set Thumbprint=%%i
 
 if errorlevel 1 (
-    echo Certificate with subject containing "Mega Limited" not found in the "%CERT_STORE%" store.
+    echo Certificate with subject containing %CERT_SUBJECT% not found in the "%CERT_STORE%" store.
     exit /b
 ) else (
     echo Certificate found. Proceeding with signing.
@@ -58,14 +65,27 @@ if not exist "%MSIX_PATH%" (
     exit /b
 )
 
+echo.
 :: Sign the DLL file
 signtool sign /sha1 "%Thumbprint%" /td SHA256 /fd SHA256 %DLL_PATH%
 
+:: Verify signature
+signtool verify /pa "%DLL_PATH%"
+if %errorlevel% neq 0 (
+    echo Error: DLL signing has failed.
+    exit /b %errorlevel%
+)
+
+echo.
 :: Sign the MSIX file
 signtool sign /sha1 "%Thumbprint%" /td SHA256 /fd SHA256 %MSIX_PATH%
 
-:: Verify the signed files
-signtool verify /pa "%DLL_PATH%"
+:: Verify signature
 signtool verify /pa "%MSIX_PATH%"
+if %errorlevel% neq 0 (
+    echo Error: DMSIX signing has failed.
+    exit /b %errorlevel%
+)
 
+echo.
 echo Files signed successfully.
