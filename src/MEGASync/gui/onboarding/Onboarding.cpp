72ccded8d332c5990fbbfc9c2915141b107b3348
Josep Subirana Oller <jso@mega.co.nz>
2025-03-20 15:57:12 +0100

    SNC-5352: Onboarding dialog now uses SyncsComponent

--- src/MEGASync/gui/onboarding/Onboarding.cpp
+++ src/MEGASync/gui/onboarding/Onboarding.cpp
@@ -16,7 +16,7 @@ using namespace mega;
 
 Onboarding::Onboarding(QObject* parent):
     QMLComponent(parent),
-    mSyncs(std::make_unique<Syncs>())
+    mSyncsComponent(std::make_unique<SyncsComponent>())
 {
     qmlRegisterModule("Onboarding", 1, 0);
     qmlRegisterType<OnboardingQmlDialog>("OnboardingQmlDialog", 1, 0, "OnboardingQmlDialog");
@@ -25,14 +25,7 @@ Onboarding::Onboarding(QObject* parent):
     qmlRegisterUncreatableType<SettingsDialog>("SettingsDialog", 1, 0, "SettingsDialog",
                                                QString::fromUtf8("Warning SettingsDialog : not allowed to be instantiated"));
 
-    SyncsComponent::registerQmlModules();
     BackupCandidatesComponent::registerQmlModules();
-
-    QmlManager::instance()->setRootContextProperty(QString::fromLatin1("syncsComponentAccess"),
-                                                   this);
-
-    auto syncsData = mSyncs->getSyncsData();
-    QmlManager::instance()->setRootContextProperty(QString::fromLatin1("syncsData"), syncsData);
 
     // Makes the Guest window transparent (macOS)
     QQuickWindow::setDefaultAlphaBuffer(true);
@@ -52,32 +45,4 @@ bool Onboarding::deviceNameAlreadyExists(const QString& name) const
 {
     // TODO : SAT-1645 implement this
     return false;
-}
-
-/*
- * public slots to be called from qml code
- */
-void Onboarding::addSync(SyncInfo::SyncOrigin origin, const QString& local, const QString& remote)
-{
-    mSyncs->addSync(origin, local, remote);
-}
-
-bool Onboarding::checkLocalSync(const QString& path)
-{
-    return mSyncs->checkLocalSync(path);
-}
-
-bool Onboarding::checkRemoteSync(const QString& path)
-{
-    return mSyncs->checkRemoteSync(path);
-}
-
-void Onboarding::clearRemoteError()
-{
-    mSyncs->clearRemoteError();
-}
-
-void Onboarding::clearLocalError()
-{
-    mSyncs->clearLocalError();
 }
