b746bcd79f9b76f196a36c5fd38c8a2b9bf05bb1
Mauro Ferreiro <mfm@mega.co.nz>
2025-03-21 13:24:55 +0100

    Sign is an argument and not a env variable

--- sign_products.cmd
+++ sign_products.cmd
@@ -1,13 +1,14 @@
-if not "%MEGA_SIGN_PRODUCTS%"=="" if /I "%MEGA_SIGN_PRODUCTS%"=="true" (
+IF "%1" EQU "true" (
+	REM Sign products
 
-REM Sign products
+	call sign_modern_context_menu_products.cmd --path "build-x64-windows-mega\src\MEGAShellExt\RelWithDebInfo" || exit 1 /b
 
-call sign_modern_context_menu_products.cmd --path "build-x64-windows-mega\src\MEGAShellExt\RelWithDebInfo" || exit 1 /b
+	IF "%MEGA_SKIP_32_BIT_BUILD%" == "true" (
+		GOTO :EOF
+	)
 
-IF "%MEGA_SKIP_32_BIT_BUILD%" == "true" (
-	GOTO :EOF
+	call sign_modern_context_menu_products.cmd --path "build-x86-windows-mega\src\MEGAShellExt\RelWithDebInfo" || exit 1 /b
+) ELSE (
+	echo "Sign products skipped."
 )
 
-call sign_modern_context_menu_products.cmd --path "build-x86-windows-mega\src\MEGAShellExt\RelWithDebInfo" || exit 1 /b
-
-)
