0651a4fb8fea040d9b604c0c18a0a5f9c0ec3af9
Raphaël Lizé <rl@mega.nz>
2025-04-29 19:04:35 +0200

    Pro urls: put parameter at the end

--- src/MEGASync/gui/upsell/UpsellController.cpp
+++ src/MEGASync/gui/upsell/UpsellController.cpp
@@ -24,6 +24,7 @@ constexpr long long TRANSFER_REMAINING_TIME_INTERVAL_MS(1000ll);
 constexpr int64_t NB_B_IN_1GB(1024 * 1024 * 1024);
 constexpr QLatin1Char BILLING_CURRENCY_REMARK('*');
 constexpr const char* DEFAULT_PRO_URL("mega://#pro");
+constexpr const char* PERIOD_SUFFIX_URL("?m=");
 const std::map<int, const char*> PRO_LEVEL_TO_URL = {
     {Preferences::AccountType::ACCOUNT_TYPE_PROI,      "mega://#propay_1"  },
     {Preferences::AccountType::ACCOUNT_TYPE_PROII,     "mega://#propay_2"  },
@@ -481,6 +482,9 @@ QUrl UpsellController::getUpsellPlanUrl(int proLevel)
 
     QString planUrlString(QString::fromLatin1(planUrlChar));
     Utilities::getPROurlWithParameters(planUrlString);
+    planUrlString += QString::fromLatin1(PERIOD_SUFFIX_URL);
+    planUrlString +=
+        (mPlans->isMonthly() ? QString::number(MONTH_PERIOD) : QString::number(YEAR_PERIOD));
 
     return QUrl(planUrlString);
 }
