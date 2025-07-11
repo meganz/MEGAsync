9510b3eb39f5c640fd0d7c075648282aefeb9782
Raphaël Lizé <rl@mega.nz>
2025-07-11 12:04:48 +0200

    Replace string by new one

--- src/MEGASync/gui/translations/translation.source.ts
+++ src/MEGASync/gui/translations/translation.source.ts
@@ -10054,11 +10054,6 @@ Please delete the folder sync from settings to cancel them.</source>
         <translatorcomment>Label to inform the user about why the account is locked and needs verification (email verification)</translatorcomment>
         <translation>Why am I seeing this?</translation>
     </message>
-    <message>
-        <source>Your account has been temporarily suspended for your safety. Please verify your email and follow its steps to unlock your account.</source>
-        <translatorcomment>Label that indicates to the user that the account has been blocked CON-465</translatorcomment>
-        <translation>Your account has been temporarily suspended for your safety. Please verify your email and follow its steps to unlock your account.</translation>
-    </message>
 </context>
 <context>
     <name>WhatsNewStrings</name>
