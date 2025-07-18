ace40f21abf0e00ced72224006c92cf3fc8c3c06
Ekaitz Privado <ep@mega.co.nz>
2025-07-18 13:34:49 +0200

    mr comments fix

--- src/MEGASync/main.cpp
+++ src/MEGASync/main.cpp
@@ -542,11 +542,20 @@ int main(int argc, char *argv[])
     qInstallMessageHandler(messageHandler);
 
     app.setStyle(new MegaProxyStyle());
-    QFile file(QString::fromLatin1(":/style/WidgetsComponentsStyleSheetsSizes.css"));
-    file.open(QIODevice::ReadOnly | QIODevice::Text);
-    QString sourceStandardComponentsStyleSheet = QString::fromLatin1(file.readAll());
-    file.close();
-    app.setStyleSheet(sourceStandardComponentsStyleSheet);
+    QFile file(QLatin1String(":/style/WidgetsComponentsStyleSheetsSizes.css"));
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QString sourceStandardComponentsStyleSheet = QString::fromLatin1(file.readAll());
+        file.close();
+        app.setStyleSheet(sourceStandardComponentsStyleSheet);
+    }
+    else
+    {
+        MegaApi::log(MegaApi::LOG_LEVEL_INFO,
+                     QString::fromUtf8("Couldn´t open WidgetsComponentsStyleSheetsSizes.css")
+                         .toUtf8()
+                         .constData());
+    }
 
 #ifdef Q_OS_MACX
 
