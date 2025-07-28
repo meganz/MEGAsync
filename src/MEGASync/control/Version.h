6895f6080b0955320c49d6e24329795c9481947e
Raphaël Lizé <rl@mega.nz>
2025-07-28 17:55:28 +0200

    Bump to v5.15.0 RC1, sdk to v9.10.0-rc.2 and update changelog

--- src/MEGASync/control/Version.h
+++ src/MEGASync/control/Version.h
@@ -4,11 +4,11 @@
 // These numbers are used in different build project scripts. Keep this in
 // mind if you want to change the format.
 #define VER_MAJOR 5
-#define VER_MINOR 14
+#define VER_MINOR 15
 #define VER_MICRO 0
-#define VER_RC 2
+#define VER_RC 0
 // Format: "VER_MAJOR.VER_MINOR.VER_MICRO.VER_RC\0"
-#define VER_PRODUCTVERSION_STR "5.14.0.2\0"
+#define VER_PRODUCTVERSION_STR "5.15.0.0\0"
 #define VER_PRODUCTVERSION VER_MAJOR, VER_MINOR, VER_MICRO, VER_RC
 #define VER_FILEVERSION VER_MAJOR, VER_MINOR, VER_MICRO, VER_RC
 #define VER_FILEVERSION_CODE (VER_MAJOR * 10000 + VER_MINOR * 100 + VER_MICRO)
@@ -26,7 +26,7 @@
 #define VER_PRODUCTNAME_STR         "MEGAsync\0"
 
 /* SDK commit hash, 7 chars */
-#define VER_SDK_ID "9d81643"
+#define VER_SDK_ID "024b12f"
 
 /* Update scrips relying on this value if you move it
 Format: 1 item by line, starting from line following the #define
@@ -38,9 +38,7 @@ Format: 1 item by line, starting from line following the #define
 )*/
 #define VER_CHANGES_NOTES \
     QT_TRANSLATE_NOOP("Preferences", \
-                      "- Increased the number of parallel transfers.\n" \
-                      "- It’s now easier to add syncs during setup.\n" \
-                      "- Added support for proxy auto-detection in Linux.\n" \
+                      "- We’ve improved error handling.\n" \
                       "- We’ve fixed bugs and made the app more reliable.\n")
 
 #endif // VERSION_H
