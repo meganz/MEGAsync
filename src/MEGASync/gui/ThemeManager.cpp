6cc4bb0c8c9693a91d103e078c58aafef12e3c44
Mauro Ferreiro <mfm@mega.co.nz>
2025-07-31 09:04:50 +0200

    Call icon-clear logic before changing the theme
    (cherry picked from commit a62141106573dccccb5685f726ae2b9e1b3a0b27)

--- src/MEGASync/gui/ThemeManager.cpp
+++ src/MEGASync/gui/ThemeManager.cpp
@@ -1,6 +1,8 @@
 #include "ThemeManager.h"
 
+#include "Platform.h"
 #include "Preferences/Preferences.h"
+#include "TextDecorator.h"
 #include "Utilities.h"
 
 const QMap<Preferences::ThemeType, QString> ThemeManager::mThemesMap = {
@@ -43,6 +45,31 @@ void ThemeManager::setTheme(Preferences::ThemeType theme)
         mCurrentTheme = theme;
 
         Preferences::instance()->setThemeType(mCurrentTheme);
+
+        if (!Platform::getInstance()->loadThemeResource(getSelectedThemeString()))
+        {
+            mega::MegaApi::log(mega::MegaApi::LOG_LEVEL_ERROR, "Error loading resource files.");
+
+            auto title = QCoreApplication::translate("MegaError", "Alert");
+            auto msg = QCoreApplication::translate(
+                "MegaError",
+                "[B]Error detected[/B][BR]An error has occurred loading application "
+                "resources, and the app needs to close. If this happens more than once, reinstall "
+                "the "
+                "app from [A]mega.io/desktop[/A] or contact support for further assistance.");
+
+            const Text::Bold boldDecorator;
+            boldDecorator.process(msg);
+
+            const Text::NewLine newLineDecorator;
+            newLineDecorator.process(msg);
+
+            const Text::Link linkDecorator(Utilities::DESKTOP_APP_URL);
+            linkDecorator.process(msg);
+
+            QMessageBox::warning(nullptr, title, msg, QMessageBox::Ok);
+            ::exit(0);
+        }
 
         emit themeChanged(theme);
         Utilities::propagateCustomEvent(ThemeChanged);
