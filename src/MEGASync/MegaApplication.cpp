c05f7d26aa9a1c9a7e646253e96337c0cc18044f
Raphaël Lizé <rl@mega.nz>
2026-04-07 13:43:57 +0200

    Do not fetch discount codes in fatal error state

--- src/MEGASync/MegaApplication.cpp
+++ src/MEGASync/MegaApplication.cpp
@@ -6958,7 +6958,7 @@ void MegaApplication::showStalledIssuesDialog()
 void MegaApplication::requestUserDiscounts(bool skipChecks)
 {
     auto logged = preferences->logged();
-    if (appfinished || !logged)
+    if (appfinished || !logged || AppState::instance()->getAppState() == AppState::FATAL_ERROR)
     {
         return;
     }
